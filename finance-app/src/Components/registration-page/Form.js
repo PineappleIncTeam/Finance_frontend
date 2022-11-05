3e84fe4e23fa096c52ea8e4091cb75ab71ec94a6
VitaliyTheKeyboardist <vital10@inbox.ru>
2022-11-05 12:52:10 +0300

    подключил jquery для передачи токена как в ссылке

--- finance-app/src/Components/registration-page/Form.js
+++ finance-app/src/Components/registration-page/Form.js
@@ -1,12 +1,17 @@
 // Форма регистрации, в разработке, будет прикрепляться к приложению
 import { useState } from 'react';
+import getCookie from './getCookie';
+// import CSRFToken from './CSRFToken'
+
+let csrftoken = getCookie('csrftoken')
 
 async function loginUser(credentials) {
   fetch('http://127.0.0.1:8000/api/drf-auth/login/', {
     method: 'POST',
-    // headers: {
-    //   'Content-Type': 'application/json',
-    // },
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': csrftoken
+    },
     body: JSON.stringify(credentials),
   }).then((loginData) => loginData.json());
   console.log(credentials);
@@ -33,6 +38,7 @@ function Form() {
     <>
       <h1>Login Form</h1>
       <form onSubmit={handleFormSubmit}>
+        
         <label>
           Username:
           <input

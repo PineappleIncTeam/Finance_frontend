28c1f451704f2b1f5dcc57760cc4f57b51dc4439
VitaliyTheKeyboardist <vital10@inbox.ru>
2022-11-06 09:59:34 +0300

    changes css

--- finance-app/src/Components/MainFieldAnalitic.js
+++ finance-app/src/Components/MainFieldAnalitic.js
@@ -2,21 +2,44 @@
 // import MainFieldString from './MainFieldString';
 
 function MainFieldAnalitic() {
-    return (
-        <div className="main_field main_field_analitic">
-          <h2 className="main_field_title">Аналитика</h2>
-            <label className="label_analitic label_analitic1">Доходы
-                <input className="main_field_string_input analitic_string" type='text'></input>
-            </label>
-            <label className="label_analitic">Расходы
-                <input className="main_field_string_input analitic_string" type='text'></input>
-            </label>
-            <label className="label_analitic">Баланс
-                <input className="main_field_string_input analitic_string" type='text'></input>
-            </label>
-          
-        </div>
-      );
+  return (
+    <div className="main_field main_field_analitic">
+      <h2 className="main_field_title">Аналитика</h2>
+      <label className="label_analitic label_analitic1">
+        Доходы
+        <input
+          className="main_field_string_input analitic_string"
+          type="text"
+        ></input>
+      </label>
+      <label className="label_analitic">
+        Расходы
+        <input
+          className="main_field_string_input analitic_string"
+          type="text"
+        ></input>
+      </label>
+      <label className="label_analitic">
+        Баланс
+        <input
+          className="main_field_string_input analitic_string"
+          type="text"
+        ></input>
+      </label>
+      <h3 className="main_field_analitic_h3">Перевести в накопления</h3>
+      <div>
+        <input
+          className="main_field_string_input analitic_string_down"
+          type="text"
+        ></input>
+        <input
+          className="main_field_string_input analitic_string_down"
+          type="text"
+        ></input>
+        <button type="submit" className="main_field_string_button analitic_button">Добавить</button>
+      </div>
+    </div>
+  );
 }
 
-export default MainFieldAnalitic
+export default MainFieldAnalitic;

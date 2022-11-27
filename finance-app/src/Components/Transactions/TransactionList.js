e4c0150415daf0ca027ed8c0d10c787a13e8d7d3
Kamal587 <kamalbugaldinov@rambler.ru>
2022-11-27 11:46:06 +0600

    доработка css

--- finance-app/src/Components/Transactions/TransactionList.js
+++ finance-app/src/Components/Transactions/TransactionList.js
@@ -1,11 +1,13 @@
+import s from "./Transactions.module.css";
+
 function TransactionList({ operationList }) {
   return (
     <div className="">
       {operationList &&
         operationList.map((operation, index) => {
           return (
-            <div className="last_operation_list" key={index}>
-              <div className="operation_list_item">{operation.date}</div>
+            <div className={s.operation} key={index}>
+              <div className={s.operation_list_item}>{operation.date}</div>
               <div className="">{operation.categoryName}</div>
               <div className="">+{operation.sum}</div>
               <div className=""></div>

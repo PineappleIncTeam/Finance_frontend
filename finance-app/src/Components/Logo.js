1a864da12d155bc23667d98e77acbff7b01aa6b1
Vitaliy <vital10@inbox.ru>
2023-01-22 12:38:26 +0300

    Logo

--- finance-app/src/Components/Logo.js
+++ finance-app/src/Components/Logo.js
@@ -2,7 +2,7 @@
 function Logo() {
     return <div className="logo_block">
         <h3 className="logo">FREEnance</h3>
-        <p className="logo_text">pineapple inc.</p>
+        <div className="logo_text"></div>
     </div>
 }
 export default Logo

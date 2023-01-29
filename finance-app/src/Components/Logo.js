eaba425167e1294bcb53a058acc9160bee740978
Kamal587 <kamalbugaldinov@rambler.ru>
2023-01-29 13:21:38 +0600

    regist mobile

--- finance-app/src/Components/Logo.js
+++ finance-app/src/Components/Logo.js
@@ -1,8 +1,10 @@
 // Компонент "Логотип"
 function Logo() {
-    return <div className="logo_block">
-        <h3 className="logo">FREEnance</h3>
-        <div className="logo_text"></div>
+  return (
+    <div className="logo_block">
+      <h3 className="logo">FREEnance</h3>
+      <div className="logo_text"></div>
     </div>
+  );
 }
-export default Logo
+export default Logo;

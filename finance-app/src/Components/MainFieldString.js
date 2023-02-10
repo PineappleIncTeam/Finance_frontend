0be4d90fa7fca34ea8d8f539a88e4f28dd68fcba
Vitaliy <vital10@inbox.ru>
2023-02-10 22:30:36 +0300

    some styles

--- finance-app/src/Components/MainFieldString.js
+++ finance-app/src/Components/MainFieldString.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setDateCalendar } from "../store/dataSlice";
-import SelectElement from "./SelectElement";
-import Dropdown from "./Dropdown/Dropdown";
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { setDateCalendar } from "../store/dataSlice"
+import SelectElement from "./SelectElement"
+import Dropdown from "./Dropdown/Dropdown"
 
 function MainFieldString({
   title,
@@ -21,35 +21,35 @@ function MainFieldString({
   getBalanceData,
   range,
 }) {
-  const token = useSelector((state) => state.user.token);
-  const dataCal = useSelector((state) => state.data.data);
+  const token = useSelector((state) => state.user.token)
+  const dataCal = useSelector((state) => state.data.data)
 
-  const [inputDis, setInputDis] = useState(false);
-  const [enterSum, setEnterSum] = useState("");
-  const [selectElement, setSelectElement] = useState({});
+  const [inputDis, setInputDis] = useState(false)
+  const [enterSum, setEnterSum] = useState("")
+  const [selectElement, setSelectElement] = useState({})
 
   function changeSelectElement(object) {
-    setSelectElement(object);
+    setSelectElement(object)
   }
 
   const disInput = () => {
-    setInputDis(true);
-  };
-  let dataCalendar = dataCal && dataCal.split(".").reverse().join("-");
+    setInputDis(true)
+  }
+  let dataCalendar = dataCal && dataCal.split(".").reverse().join("-")
 
-  let Data = new Date();
-  let Year = Data.getFullYear();
-  let Month = Data.getMonth() + 1;
-  let Day = Data.getDate();
-  let dataOnline = Year + "-" + Month + "-" + Day;
+  let Data = new Date()
+  let Year = Data.getFullYear()
+  let Month = Data.getMonth() + 1
+  let Day = Data.getDate()
+  let dataOnline = Year + "-" + Month + "-" + Day
 
   function sumSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
     let data = {
       sum: enterSum,
       category_id: selectElement.category_id,
       date: dataCalendar ? dataCalendar : dataOnline,
-    };
+    }
 
     const options = {
       method: "POST",
@@ -58,25 +58,25 @@ function MainFieldString({
         Authorization: `Token ${token}`,
       },
       body: JSON.stringify(data),
-    };
+    }
 
     fetch(typeOfSum, options)
       .then((result) => getInputData(sumCash))
       .then((serverResponse) => {
-        getBalanceData();
-        setTimeout(() => getOperationList(endpoint, symbol), 200);
-      });
-    setEnterSum("");
+        getBalanceData()
+        setTimeout(() => getOperationList(endpoint, symbol), 200)
+      })
+    setEnterSum("")
   }
 
   function handleInputChange(event) {
-    setEnterSum(event.target.value);
+    setEnterSum(event.target.value)
   }
 
   const changeHandler = (e) => {
-    const value = e.target.value;
-    e.target.value = value.replace(/[^0-9.]+/g, "");
-  };
+    const value = e.target.value
+    e.target.value = value.replace(/[^0-9.]+/g, "")
+  }
 
   return (
     <form className="main_field_string" onSubmit={sumSubmit}>
@@ -96,16 +96,18 @@ function MainFieldString({
         />
       )}
 
-      <input
-        type="text"
-        className="main_field_string_input"
-        value={enterSum}
-        onInput={(event) => changeHandler(event)}
-        min="1"
-        onChange={(event) => handleInputChange(event)}
-        disabled={Boolean(!inputDis)}
-      ></input>
-      <span className="ruble_icon ruble_icon_small">₽</span>
+      <div className="input_container">
+        <input
+          type="text"
+          className="main_field_string_input"
+          value={enterSum}
+          onInput={(event) => changeHandler(event)}
+          min="1"
+          onChange={(event) => handleInputChange(event)}
+          disabled={Boolean(!inputDis)}
+        ></input>
+        <span className="ruble_icon ruble_icon_small">₽</span>
+      </div>
 
       <button
         type="submit"
@@ -122,6 +124,6 @@ function MainFieldString({
         +
       </button>
     </form>
-  );
+  )
 }
-export default MainFieldString;
+export default MainFieldString

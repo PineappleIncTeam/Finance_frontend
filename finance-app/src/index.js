dccbd36487b05a929bff6ce8bf8771b1cac8f122
Vitaliy <vital10@inbox.ru>
2023-07-29 07:47:37 +0300

    google tag manager

--- finance-app/src/index.js
+++ finance-app/src/index.js
@@ -5,6 +5,13 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = { 
+  gtmId: 'GTM-000000' 
+} 
+
+TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

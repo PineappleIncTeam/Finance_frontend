fcc252a7aae8f2b16975e3c4b91cc8791dca3e55
AlexMakovetskiy <bo5634302@gmail.com>
2024-04-30 07:40:52 +0300

    fix/ resolve build error crush issue

--- src/app/(privateProfile)/privateProfile/page.tsx
+++ src/app/(privateProfile)/privateProfile/page.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 // import Main from "./main/page";
 
-import styles from "./page.module.css";
+import styles from "./PrivateProfile.module.css";
 
 export default function PrivateProfile() {
 	return (
-		<div className={styles.homePageWrap}>
+		<div className={styles.privateProfileWrap}>
 			<Link href="/profitArea">profitArea</Link>
 			<main>Main</main>
 		</div>

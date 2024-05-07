c79b8198ff24ed9bb81d41bd6a236c396335c76f
VitaliyTheKeyboardist <vital10@inbox.ru>
2024-05-07 19:48:44 +0300

    fix: changed class names

--- src/app/(home)/[not-found]/page.tsx
+++ src/app/(home)/[not-found]/page.tsx
@@ -8,12 +8,12 @@ function NotFound() {
 	return (
 		<main className={styles.notFoundPageWrap}>
 			<div className={styles.notFoundPageContainer}>
-				<Image className={styles.notFoundPageContainer__image} src={meditatingMan} alt="meditating man" />
-				<div className={styles.titleContainer}>
-					<h1 className={styles.titleContainer__title}>404</h1>
-					<p className={styles.titleContainer__subtitle}>Такой страницы у нас нет...</p>
-					<p className={styles.titleContainer__subtitle}>Но есть помощник финансового планирования</p>
-					<button className={styles.titleContainer__navigateAction}>Войти в приложение</button>
+				<Image className={styles.notFoundPageContainer__meditatingMan} src={meditatingMan} alt="meditating man" />
+				<div className={styles.infoContainer}>
+					<h1 className={styles.infoContainer__title}>404</h1>
+					<p className={styles.infoContainer__subtitle}>Такой страницы у нас нет...</p>
+					<p className={styles.infoContainer__subtitle}>Но есть помощник финансового планирования</p>
+					<button className={styles.infoContainer__navigateAction}>Войти в приложение</button>
 				</div>
 			</div>
 		</main>

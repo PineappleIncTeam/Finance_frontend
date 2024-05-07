b7deac9575d4af9041e4955fb884b8c33a1fe4b2
VitaliyTheKeyboardist <vital10@inbox.ru>
2024-05-07 20:41:02 +0300

    fix: add Link tag from next/link

--- src/app/(home)/[not-found]/page.tsx
+++ src/app/(home)/[not-found]/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import meditatingMan from "../../../assets/pages/notFound/meditatingMan.png";
 
@@ -13,7 +14,9 @@ function NotFound() {
 					<h1 className={styles.infoContainer__title}>404</h1>
 					<p className={styles.infoContainer__subtitle}>Такой страницы у нас нет...</p>
 					<p className={styles.infoContainer__subtitle}>Но есть помощник финансового планирования</p>
-					<button className={styles.infoContainer__navigateAction}>Войти в приложение</button>
+					<Link href="/LoginPage">
+						<button className={styles.infoContainer__navigateAction}>Войти в приложение</button>
+					</Link>
 				</div>
 			</div>
 		</main>

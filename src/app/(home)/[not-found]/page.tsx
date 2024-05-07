51cf732dae330ae88b2d3e9740feee04f2cdddbd
VitaliyTheKeyboardist <vital10@inbox.ru>
2024-05-07 15:19:40 +0300

    init: create not-found page structure

--- src/app/(home)/[not-found]/page.tsx
+++ src/app/(home)/[not-found]/page.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+
+import meditatingMan from "../../../../public/meditatingMan.svg";
+
+import styles from "./page.module.css";
+
 function NotFound() {
-	return <div>NotFound</div>;
+	return (
+		<main className={styles.notFoundPageWrap}>
+			<div className={styles.notFoundPageContainer}>
+				<Image className={styles.notFoundPageContainer__image} src={meditatingMan} alt="meditating man" />
+				<div className={styles.titleContainer}>
+					<h1 className={styles.titleContainer__title}>404</h1>
+					<p className={styles.titleContainer__subtitle}>Такой страницы у нас нет...</p>
+					<p className={styles.titleContainer__subtitle}>Но есть помощник финансового планирования</p>
+					<button className={styles.titleContainer__navigateAction}>Войти в приложение</button>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default NotFound;

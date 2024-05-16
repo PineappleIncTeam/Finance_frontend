bf22803bffc91f56b5790bef8119d85847f79c76
Zhdanovich <Zhiv80@mail.ru>
2024-05-16 12:18:36 +0300

    feat: add mobile styles

--- src/app/(home)/serverError/page.tsx
+++ src/app/(home)/serverError/page.tsx
@@ -2,15 +2,15 @@ import style from "./serverError.module.css";
 
 export default function ServerErrorPage() {
 	return (
-		<div className={style.serverError}>
-			<div className={style.serverError__textWrapper}>
-				<h1 className={style.serverError__title}>Ошибка на сервере...</h1>
-				<p className={style.serverError__text}>Приложение еще активно разрабатывается и ошибки случаются...</p>
-				<p className={style.serverError__text}>
+		<div className={style.serverErrorWrap}>
+			<div className={style.serverErrorContainer}>
+				<h1 className={style.serverErrorContainer__title}>Ошибка на сервере...</h1>
+				<p className={style.serverErrorContainer__subtitle}>Приложение еще активно разрабатывается и ошибки случаются...</p>
+				<p className={style.serverErrorContainer__subtitle}>
 					Пока Вы это читаете, по тревоге были подняты наши программисты и администраторы, которые уже прочитали
 					подробный отчет об этой ошибке и приступили к ее устранению.
 				</p>
-				<p className={style.serverError__text}>В ближайшее время она будет исправлена!</p>
+				<p className={style.serverErrorContainer__subtitle}>В ближайшее время она будет исправлена!</p>
 			</div>
 		</div>
 	);

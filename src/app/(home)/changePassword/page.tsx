2230cf0db1eaa786241339047c8648224572134f
Zhdanovich <Zhiv80@mail.ru>
2024-05-22 11:57:38 +0300

    init: creat Changing password page fullHD

--- src/app/(home)/changePassword/page.tsx
+++ src/app/(home)/changePassword/page.tsx
@@ -4,11 +4,13 @@ import { LetterIcon } from "../../../assets/pages/changePassword/LetterIcon";
 
 import { PaperAirLineIcon } from "../../../assets/pages/changePassword/PaperAirLineIcon";
 import { OpenLetterIcon } from "../../../assets/pages/changePassword/OpenLetterIcon";
-import { ArrowsIcon } from "../../../assets/pages/changePassword/Arrows";
+import { ArrowsIcon } from "../../../assets/pages/changePassword/ArrowsIcon";
 import { QuestionIcon } from "../../../assets/pages/changePassword/QuestionIcon";
 import { EmailIcon } from "../../../assets/pages/changePassword/EmailIcon";
 
 import style from "./changePassword.module.css";
+import { OvalIcon } from "../../../assets/pages/changePassword/OvalIcon";
+import { ManIcon } from "../../../assets/pages/changePassword/ManIcon";
 
 // interface ChangePasswordProps {
 // 	onSubmit: () => void;
@@ -18,13 +20,15 @@ export default function changePassword(ChangePasswordProps: { onSubmit: FormEven
 	return (
 		<div className={style.changePasswordWrap}>
 			<div className={style.changePasswordContainer}>
+				<OvalIcon classNames={style.ovalIcon}/>
 				<QuestionIcon classNames={style.questionIcon} />
 				<EmailIcon classNames={style.emailIcon} />
-				<ArrowsIcon />
-				<LetterIcon />
+				<ArrowsIcon classNames={style.arrowsIcon}/>
+				<LetterIcon classNames={style.letterIcon}/>
 				<OpenLetterIcon classNames={style.openLetterIcon} />
+				<ManIcon classNames={style.man}/>
 				<PaperAirLineIcon classNames={style.paperAirLineIcon} />
-
+				<div className={style.changePasswordContainer__modal}>
 				<div className={style.changePasswordContainer__form}>
 					<h1 className={style.changePasswordContainer__form__title}>Изменение пароля</h1>
 					<form onSubmit={ChangePasswordProps.onSubmit}>
@@ -33,7 +37,7 @@ export default function changePassword(ChangePasswordProps: { onSubmit: FormEven
 							<input className={style.changePasswordRow} placeholder="Пароль" />
 						</label>
 						<p className={style.changePasswordHelper}>
-							Пароль должен состоять из 6 из более символов, среди которых хотя бы одна буква верхнего регистра и хотя
+							Пароль должен состоять из 6 и более символов, среди которых хотя бы одна буква верхнего регистра и хотя
 							бы одна цифра
 						</p>
 						<label className={style.label}>
@@ -43,6 +47,7 @@ export default function changePassword(ChangePasswordProps: { onSubmit: FormEven
 						<input className={style.saveButton} type="submit" value="Сохранить" />
 					</form>
 				</div>
+				</div>				
 			</div>
 		</div>
 	);

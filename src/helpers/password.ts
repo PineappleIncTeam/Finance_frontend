25aaf17c10d53c9845034b2cb105664879598e5e
AlexMakovetskiy <bo5634302@gmail.com>
2024-06-26 13:46:31 +0300

    feat: add updated regular expressions

--- src/helpers/password.ts
+++ src/helpers/password.ts
@@ -1,2 +1,2 @@
-export const emailRegex = /^\s*(?=.{8,30}$)(?!..{2})(?![-.])[\w.-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)?.[a-zA-Z]{2,6}\s$/i;
-export const passwordRegex = /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?!.[^a-zA-Z\d!#/='<>{}@,.$%[]^&*()~-:;+?])[^\s]{6,50}$/;
+export const emailRegex =  /^\s*(?=.{8,30}$)(?!.*\.{2})(?!.*--)[\w.-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)?\.[a-zA-Z]{2,6}\s*$/i;
+export const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*[^a-zA-Z\d!#/='<>{}@,.$%[\]^&*()~\-:;+?])\S{6,50}$/;

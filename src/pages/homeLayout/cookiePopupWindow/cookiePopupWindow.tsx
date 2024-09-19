33b446a0f8e9b855953e6cbd03f33a819889f8f0
VitaliyTheKeyboardist <vital10@inbox.ru>
2024-09-19 16:48:47 +0300

    init: new modal window styles

--- src/pages/homeLayout/cookiePopupWindow/cookiePopupWindow.tsx
+++ src/pages/homeLayout/cookiePopupWindow/cookiePopupWindow.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import cn from "classnames";
 
 import Button from "../../../ui/button/button";
-import { MainPath } from "../../../services/router/routes";
 import { cookieLinkTitle } from "../../../mocks/cookiePopupWindow";
-
-import closeIcon from "../../../assets/pages/mainPage/closeIcon.svg";
 
 import styles from "./cookiePopupWindow.module.scss";
 
@@ -17,13 +12,12 @@ const CookiePopupWindow = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(true);
 
 	return (
-		<div className={cn(styles.popupContainer, { [styles.popupContainer_hidden]: !isOpen })}>
-			<div className={styles.textContentBlock}>
+		<div
+			className={cn(styles.popupContainer, { [styles.popupContainer_hidden]: !isOpen })}
+			onClick={() => setIsOpen(false)}>
+			<div className={styles.textContentBlock} onClick={(e) => e.stopPropagation()}>
 				<div className={styles.textContentBlock__titleBlock}>
 					<h3 className={styles.textContentBlock__title}>Файлы cookies</h3>
-					<button type="button" className={styles.textContentBlock__action} onClick={() => setIsOpen(false)}>
-						<Image src={closeIcon} alt="крестик" />
-					</button>
 				</div>
 				<div className={styles.textContentBlock__subtitle}>
 					Все на сайте — для вас,{" "}
@@ -32,9 +26,7 @@ const CookiePopupWindow = () => {
 					</a>{" "}
 					— для нас. Собираем их, чтобы сделать наш сайт еще удобнее. Ограничить или настроить их можно в браузере.
 				</div>
-				<Link href={MainPath.SignUp} className={styles.textContentBlock__linkButton}>
-					<Button content="Регистрация" styleName="buttonForRegistration" />
-				</Link>
+				<Button content="Хорошо, принимаю" styleName="buttonConfirmCookies" onClick={() => setIsOpen(false)} />
 			</div>
 		</div>
 	);

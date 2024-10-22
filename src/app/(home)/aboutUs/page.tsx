57b68938a025eb1906a7f26caf3c2379abca1273
Faz-R <FazaR92@gmail.com>
2024-10-23 01:57:22 +0300

    init: add structure to about us page

--- src/app/(home)/aboutUs/page.tsx
+++ src/app/(home)/aboutUs/page.tsx
@@ -1,5 +1,14 @@
+import Footer from "../../../components/mainLayout/footer/footer";
+
+import style from "./aboutUs.module.scss";
+
 function AboutUs() {
-	return <div />;
+	return (
+		<div className={style.aboutUsWrap}>
+			<div className={style.aboutUsContainer}></div>
+			<Footer />
+		</div>
+	);
 }
 
 export default AboutUs;

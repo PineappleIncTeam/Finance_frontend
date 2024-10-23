ebc313dc8a6a885f09fb1960c56b541615079404
Faz-R <FazaR92@gmail.com>
2024-10-23 03:57:13 +0300

    feat: add profile card

--- src/app/(home)/aboutUs/page.tsx
+++ src/app/(home)/aboutUs/page.tsx
@@ -1,12 +1,31 @@
+import Image from "next/image";
+
 import Footer from "../../../components/mainLayout/footer/footer";
+
+import defoltUser from "../../../assets/pages/aboutUs/defolt-profile.svg";
 
 import style from "./aboutUs.module.scss";
 
 function AboutUs() {
 	return (
 		<div className={style.aboutUsWrap}>
-			<div className={style.aboutUsContainer}></div>
-			<Footer />
+			<div className={style.aboutUsContainer}>
+				<div className={style.aboutUsMain}>
+					<div className={style.aboutUsTitle}>Классные ребята с горящими глазами.</div>
+					<div className={style.aboutUsCards}>
+						<div className={style.aboutUsCard}>
+							<div className={style.aboutUsCardPictureWrap}>
+								<Image src={defoltUser} alt={`${defoltUser}`} className={style.aboutUsCardPicture} />
+							</div>
+							<div className={style.aboutUsCardInfo}>
+								<div className={style.aboutUsCardInfoLine} />
+								<div className={style.aboutUsCardInfoVacancy}>Product owner</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<Footer />
+			</div>
 		</div>
 	);
 }

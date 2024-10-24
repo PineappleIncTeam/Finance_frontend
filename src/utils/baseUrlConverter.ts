6d4146e8cd1f27a77bd0346e8887f06a4b5c6b78
AlexMakovetskiy <bo5634302@gmail.com>
2024-10-24 18:30:19 +0300

    feat: update env util for localhost

--- src/utils/baseUrlConverter.ts
+++ src/utils/baseUrlConverter.ts
@@ -1,4 +1,8 @@
 export function getCorrectBaseUrl() {
+	if (window.location.hostname === "localhost") {
+		return "https://dev.freenance.store";
+	}
+
 	const envDomainName = window.location.hostname.split(".")[0];
 	return `https://${envDomainName}.freenance.store`;
 }

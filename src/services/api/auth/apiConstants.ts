4a0dd5dfb2e1682d5a45fff5247ed023b994da7c
AlexMakovetskiy <bo5634302@gmail.com>
2024-10-24 13:01:29 +0300

    feat: add correct base url util

--- src/services/api/auth/apiConstants.ts
+++ src/services/api/auth/apiConstants.ts
@@ -1,4 +1,6 @@
-export const baseURL = "https://dev.freenance.store";
+import { getCorrectBaseUrl } from "../../../utils/baseUrlConverter";
+
+export const baseURL = getCorrectBaseUrl();
 
 export const signupEndpoint = "api/v1/auth/users/";
 export const signInEndpoint = "api/v1/auth/token/login/";

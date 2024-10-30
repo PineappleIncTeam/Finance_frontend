5b5e60b7f61275df401366d174be22ae735ac0ff
Faz-R <FazaR92@gmail.com>
2024-10-31 00:40:53 +0300

    init: add agreement page

---
+++ src/app/(home)/agreement/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import agreement from "../../../mocks/Agreement.json";
+
+import style from "./agreement.module.scss";
+
+function AgreementPage() {
+	return (
+		<div className={style.AgreementPageWrap}>
+			<div className={style.AgreementPageContainer}>
+				<div className={style.AgreementPageContent}>
+					<h1 className={style.AgreementArticleTitle}>Привет</h1>
+					{agreement.userAgreement.management}
+					{agreement.userAgreement.management}
+					{agreement.userAgreement.management}
+					{agreement.userAgreement.management}
+					{agreement.userAgreement.management}
+					{agreement.userAgreement.management}
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum vel dicta cumque beatae laborum laboriosam
+					assumenda nisi vitae ullam earum, perspiciatis deserunt molestias velit repellendus iste eaque, aperiam
+					excepturi dolorum! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam natus vitae perspiciatis
+					asperiores obcaecati! Repellendus labore delectus quae fugit tenetur dignissimos ab laboriosam blanditiis
+					dicta saepe, officia id quisquam sit. Ea ratione nemo omnis modi atque ullam tempore, aspernatur libero illo
+					assumenda ipsum fugit temporibus aperiam labore commodi alias incidunt beatae, odio corrupti impedit eos
+					voluptatibus eaque doloribus iusto? Voluptatibus? Dolore adipisci tempore debitis odio officia nam eligendi
+					voluptatum quidem dolorum. Quo odio perspiciatis, dolorem reprehenderit aut explicabo nemo earum ea? Sint
+					doloribus atque expedita magni eaque debitis quae aspernatur! Distinctio repellendus consequuntur assumenda
+					eligendi iure ea, sunt doloribus numquam nostrum, quaerat perspiciatis praesentium? Veniam repellat excepturi
+					consectetur cumque id officiis veritatis, quibusdam, ad nemo labore ex fugit qui corrupti! Libero autem iure
+					asperiores aliquam optio earum esse nesciunt molestias deleniti, explicabo dignissimos architecto, ex amet
+					vitae beatae maxime porro ratione neque unde deserunt ipsum commodi impedit consequatur qui? Incidunt? Ullam
+					obcaecati nam dolorem excepturi veritatis ipsum asperiores ut optio odio. Id est sunt earum quos, ipsa error
+					provident accusamus quod tenetur neque et accusantium, deserunt omnis, doloribus illum explicabo. Ratione
+					deserunt voluptatibus amet, incidunt quis minima accusantium ipsam labore quas eum, corrupti doloribus cumque
+					atque quo facilis nobis facere voluptas repellat assumenda eaque excepturi tenetur inventore rem ducimus!
+					Quia? Harum sit corrupti nemo id sed velit eum rem incidunt iure, cumque dolor. Laboriosam quisquam esse sunt,
+					veniam vero explicabo, suscipit quo hic consectetur possimus cum odio quod sint fugit? Repellat dolorem
+					ducimus voluptatum ratione ipsam? Quidem ipsam vero voluptatem dolor qui quisquam perferendis. Nisi quae fugit
+					temporibus voluptate cupiditate, doloremque velit unde sapiente fugiat, et, explicabo delectus eos nobis.
+					Animi necessitatibus culpa voluptatibus voluptatem temporibus provident labore numquam in odio placeat amet
+					porro exercitationem, nulla eos reiciendis officiis perferendis quasi magnam asperiores natus facilis ut
+					assumenda deserunt quidem? Magni!
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default AgreementPage;

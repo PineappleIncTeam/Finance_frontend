7fd6d9d54aad11ce7ed0dbe627ba0ad64970cddc
Zhdanovich <Zhiv80@mail.ru>
2024-10-31 13:51:36 +0300

    Fix: add fallback image, add styles

--- src/components/mainLayout/youtubeEmbed/youtubeEmbed.tsx
+++ src/components/mainLayout/youtubeEmbed/youtubeEmbed.tsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
+
+import Image from "next/image";
+
 import { IYoutubeEmbed } from "../../../types/components/ComponentsTypes";
+
+import fallback from "../../../assets/components/youtubeEmbed/fallback.png";
 
 import style from "./youtubeEmbed.module.scss";
 
-const YoutubeEmbed = ({ embedId }: IYoutubeEmbed) => (
-	<div className={style.videoResponsive}>
-		<iframe
-			width="853"
-			height="480"
-			src={`https://www.yuotube.com/embed/${embedId}`}
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-			allowFullScreen
-			title="Embedded youtube"
-		/>
-	</div>
-);
+const YoutubeEmbed = ({ embedId }: IYoutubeEmbed) => {
+	const [hasError, setHasError] = useState(true);
+
+	return (
+		<div className={style.videoResponsive}>
+			{hasError ? (
+				<div className={style.fallbackImageWrapper}>
+					<Image className={style.fallbackImage} src={fallback} alt="видео отсутствует" />
+					<h1 className={style.fallbackImage__Text}>Приносим извинения. Данное видео отсутствует.</h1>
+				</div>
+			) : (
+				<iframe
+					width="854"
+					height="480"
+					src={`https://www.youtube.com/embed/${embedId}`}
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowFullScreen
+					title="Embedded youtube"
+					onError={() => setHasError(true)}
+				/>
+			)}
+		</div>
+	);
+};
 
 export default YoutubeEmbed;

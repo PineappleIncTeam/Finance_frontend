786d84af46ed3669a70df0bfeac114c385805ac5
Sofi <myfamily.sofi@gmail.com>
2024-10-31 21:18:18 +0100

    remember cooking answer

--- src/services/redux/index.ts
+++ src/services/redux/index.ts
@@ -1,14 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import dataReducer from "./features/infoPart/InfoPartSlice";
 import statusReducer from "./features/cookieStatus/cookieStatusSlice";
 
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["status"],
+};
+
+const persistedStatusReducer = persistReducer(persistConfig, statusReducer);
+
 const store = configureStore({
 	reducer: {
 		data: dataReducer,
-		status: statusReducer,
+		status: persistedStatusReducer,
 	},
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
+
 export default store;

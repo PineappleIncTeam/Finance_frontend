cc0a0f288b5942469c6e73154b602e3dc8f0bc91
AlexMakovetskiy <108091268+AlexMakovetskiy@users.noreply.github.com>
2024-11-05 10:07:51 +0300

    Update authConstants.ts

--- src/helpers/authConstants.ts
+++ src/helpers/authConstants.ts
@@ -1,4 +1,4 @@
-export const emailPattern = /^\s*(?=.{8,30}$)(?!.*\.\.)[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\s*$/i;
+export const emailPattern = /^\s*(?=.{8,30}$)(?!.*\.\.)[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+\s*$/i;
 export const passwordPattern =
 	/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*[^a-zA-Z\d!#/='<>{}@,.$%[\]^&*()~:;+?^-])[^\s]{6,50}$/;
 

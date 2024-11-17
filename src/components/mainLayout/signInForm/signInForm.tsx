fef5727dc3711bf71746f20790418f98b6cad116
AlexMakovetskiy <bo5634302@gmail.com>
2024-11-17 16:51:23 +0300

    feat: change transition page for successful authorization

--- src/components/mainLayout/signInForm/signInForm.tsx
+++ src/components/mainLayout/signInForm/signInForm.tsx
@@ -72,7 +72,7 @@ const SignInForm = () => {
 
 	const handleModalClose = () => {
 		setIsOpen(false);
-		router.push(UserProfilePath.Profile);
+		router.push(UserProfilePath.Profit);
 	};
 
 	return (

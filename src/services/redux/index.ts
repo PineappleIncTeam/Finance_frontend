0328ca7b758d83b6a2970adb5eae32661b88d07f
AlexMakovetskiy <bo5634302@gmail.com>
2024-12-01 17:36:50 +0300

    fix: resolve issue with status reducer bug

--- src/services/redux/index.ts
+++ src/services/redux/index.ts
@@ -1,17 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist";
 
 import dataReducer from "./features/infoPart/InfoPartSlice";
 import statusReducer from "./features/userStorageSettings/userStorageSettingsSlice";
 import persistConfig from "./persist/persistConfig";
 
-const persistedStatusReducer = persistReducer(persistConfig, statusReducer);
+const RootReducer = combineReducers({
+	data: dataReducer,
+	status: statusReducer,
+});
+
+const persistedRootReducer = persistReducer(persistConfig, RootReducer);
 
 const store = configureStore({
-	reducer: {
-		data: dataReducer,
-		status: persistedStatusReducer,
-	},
+	reducer: persistedRootReducer,
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
 			serializableCheck: { ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] },

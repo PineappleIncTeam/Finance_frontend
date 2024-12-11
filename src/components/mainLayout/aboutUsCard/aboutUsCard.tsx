e5dd9ff3a7478fccfc44b4552adc76df4e082109
Zhdanovich <Zhiv80@mail.ru>
2024-12-11 11:11:12 +0300

    Fix: add fallback photo

--- src/components/mainLayout/aboutUsCard/aboutUsCard.tsx
+++ src/components/mainLayout/aboutUsCard/aboutUsCard.tsx
@@ -1,15 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
 import Image from "next/image";
 
 import { IAboutUsCard } from "../../../types/common/ComponentsProps";
 
+import fallback from "../../../assets/components/aboutUs/photoNotAvailable.png";
+
 import style from "./aboutUsCards.module.scss";
 
 const AboutUsCard = ({ photo, teamRole }: IAboutUsCard) => {
+	const [hasError, setHasError] = useState<boolean>(true);
+
 	return (
 		<div className={style.aboutUsCard}>
-			<div className={style.aboutUsCardPictureWrap}>
-				<Image src={photo} alt={`${teamRole}`} className={style.aboutUsCardPicture} fill={true} />
-			</div>
+			{hasError ? (
+				<div className={style.aboutUsCardPictureWrap}>
+					<Image
+						src={fallback}
+						alt="no photo"
+						className={style.aboutUsCardPicture}
+						fill
+						sizes="(max-width: 1920px) 5vw, (max-width: 400px) 17.5vw"
+					/>
+				</div>
+			) : (
+				<div className={style.aboutUsCardPictureWrap}>
+					<Image
+						src={photo}
+						alt={`${teamRole}`}
+						className={style.aboutUsCardPicture}
+						fill={true}
+						onError={() => setHasError(true)}
+					/>
+				</div>
+			)}
 			<div className={style.aboutUsCardInfo}>
 				<div className={style.aboutUsCardInfoLine} />
 				<div className={style.aboutUsCardInfoVacancy}>{teamRole}</div>

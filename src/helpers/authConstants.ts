e7eee62cff17b9194d24a1cd676cbcaf60b40d88
Zhdanovich <Zhiv80@mail.ru>
2024-12-21 18:14:14 +0300

    Fix: correct emailPattern

--- src/helpers/authConstants.ts
+++ src/helpers/authConstants.ts
@@ -1,4 +1,5 @@
-export const emailPattern = /^\s*(?=.{8,30}$)(?!.*\.\.)[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+\s*$/i;
+export const emailPattern =
+	/^\s*(?=.{8,30}$)(?!.*\.\.)[a-zA-Z0-9_.+-`~½=!#%^|$*{}&’']+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+\s*$/i;
 export const passwordPattern =
 	/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*[^a-zA-Z\d!#/\\='"<>{}@,.$%[\]^&*()~_|:;+?^-])[^\s]{6,50}$/;
 

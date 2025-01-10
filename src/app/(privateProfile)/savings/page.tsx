4f196337f940a21d23546f9018e0a288358243ad
DenisBardashevich <ackapbinka@mail.ru>
2025-01-11 02:03:53 +0300

    feat: create selection for category

--- src/app/(privateProfile)/savings/page.tsx
+++ src/app/(privateProfile)/savings/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { useForm } from "react-hook-form";
 
+import { PlusIcon } from "../../../assets/script/expenses/PlusIcon";
+
 import AppInput from "../../../ui/appInput/AppInput";
 import { InputTypeList } from "../../../helpers/Input";
 import { ISavingsInputForm } from "../../../types/pages/Savings";
+import { Select } from "../../../ui/select/Select";
+import Button from "../../../ui/button/button";
 
 import style from "./savings.module.scss";
 
@@ -30,6 +34,30 @@ function Savings() {
 							<p className={style.dateSelectionWrapper__description}>Выбор даты</p>
 							<AppInput control={control} label={"Выбор даты"} type={InputTypeList.Date} name={"date"} />
 						</div>
+					</div>
+					<div className={style.savingsDetailsContainer}>
+						<div className={style.savingsDetailsContainer__category}>
+							<Select
+								name={"expenses"}
+								label={"Накопления"}
+								options={["Обучение ребенка", "Машина", "Квартира", "Отпуск 2024"]}
+							/>
+						</div>
+						<div className={style.savingsDetailsContainer__sum}>
+							<AppInput
+								control={control}
+								label={"Сумма"}
+								type={InputTypeList.Number}
+								name={"number"}
+								placeholder={"0.00 ₽"}
+							/>
+						</div>
+
+						{/* styleName переделать! */}
+
+						<Button content={"Добавить"} styleName={"buttonForIncome__disabled"}>
+							<PlusIcon classNames={style.addButtonIcon} />
+						</Button>
 					</div>
 				</form>
 			</div>

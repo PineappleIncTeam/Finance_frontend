8810a9b54334922e65a5487f1c77113e8f2d0c9b
NausAlexus <naus.lexus@mail.ru>
2025-01-10 16:15:19 +0300

    feat: creating input with range

--- src/app/(privateProfile)/calculator/page.tsx
+++ src/app/(privateProfile)/calculator/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
-import style from "./calculator.module.scss"
+import React, { useState } from "react";
+
+import style from "./calculator.module.scss";
 
 export default function Calculator() {
+
+	const [value, setValue] = useState(0); // Значение полузнка
+
+    const handleInputChange = (event) => {
+        const newValue = event.target.value;
+        setValue(newValue);
+    };
+
+    const handleRangeChange = (event) => {
+        const newValue = event.target.value;
+        setValue(newValue);
+    };
+
 	return (
 		<div className={style.calculatorPageWrap}>
 
@@ -36,9 +51,39 @@ export default function Calculator() {
 
 					<form className={style.calculatorFormContentWrapper}>
 					
-						<div></div>
-
-						<div></div>
+						<div className={style.selectionOfCalculationContainer}>
+							<input name="select-of-calculation" type="radio" id="selectionOfCalculationContainer__real" className={style.selectionOfCalculationContainer__input} defaultChecked/>
+							<label htmlFor="selectionOfCalculationContainer__real" className={style.selectionOfCalculationContainer__label}>
+								<p className={style.selectionOfCalculationContainer__text}>Недвижимость</p>
+							</label>
+							<input name="select-of-calculation" type="radio" id="selectionOfCalculationContainer__credit" className={style.selectionOfCalculationContainer__input}/>
+							<label htmlFor="selectionOfCalculationContainer__credit" className={style.selectionOfCalculationContainer__label}>
+								<p className={style.selectionOfCalculationContainer__text}>Кредит</p>
+							</label>
+						</div>
+						
+						<div className={style.inputRangeContainer}>
+							<label className={style.inputRangeContainer__labl} htmlFor="q1">Стоимость недвижимости, ₽</label>
+							<div className={style.inputRangeContainer1}>
+								<input 
+									type="number" 
+									value={value} 
+									onChange={handleInputChange} 
+									className={style.appInput1} 
+									min="0" 
+									max="1000000" 
+									id="q1"
+								/>
+								<input 
+									type="range" 
+									value={value} 
+									onChange={handleRangeChange} 
+									className={style.rangeInput1} 
+									min="0" 
+									max="1000000" 
+								/>
+							</div>
+						</div>
 
 						<div></div>
 

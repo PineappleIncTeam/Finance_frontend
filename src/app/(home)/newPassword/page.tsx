934b4c432b6c0e5e0e8cf66e40488b8c1819c6bb
Zhdanovich <Zhiv80@mail.ru>
2025-01-11 12:38:45 +0300

    Fix: delete unnecesary icons, add ui component, add api, add interface, correct interface, add api consts, rename 2 files, correct form, change styles, add api query

--- src/app/(home)/newPassword/page.tsx
+++ src/app/(home)/newPassword/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+
+import { useRouter } from "next/navigation";
+import { AxiosError } from "axios";
 
 import { INewPassword } from "../../../types/pages/Password";
 import AuthInput from "../../../ui/authInput/AuthInput";
@@ -11,11 +14,21 @@ import { formHelpers } from "../../../utils/formHelpers";
 import { emailPattern } from "../../../helpers/authConstants";
 import { InputTypeList } from "../../../helpers/Input";
 
+import { getCorrectBaseUrl } from "../../../utils/baseUrlConverter";
+
+import { ResetPasswordWithEmail } from "../../../services/api/auth/ResetPasswordWithEmail";
+
+import { MainPath } from "../../../services/router/routes";
+
+import { ApiResponseCode } from "../../../helpers/apiResponseCode";
+
 import style from "./newPassword.module.scss";
 
 export default function NewPassword() {
 	const [isNewPasswordModalShown, setIsNewPasswordModalShown] = useState<boolean>(false);
 	const [email, setEmail] = useState<string>("");
+	const [baseUrl, setBaseUrl] = useState<string>();
+	const seconds = 7000;
 
 	const {
 		formState: { errors },
@@ -32,12 +45,43 @@ export default function NewPassword() {
 
 	const onSubmit = (data: INewPassword) => {
 		setEmail(data?.email ?? "");
+		restoreButtonClick(data);
 		newPasswordModalVisible(true);
+		setTimeout(() => newPasswordModalVisible(false), seconds);
 		reset();
 	};
 
 	const newPasswordModalVisible = (prop: boolean) => {
 		setIsNewPasswordModalShown(prop);
+	};
+
+	useEffect(() => {
+		setBaseUrl(getCorrectBaseUrl());
+	}, []);
+
+	const router = useRouter();
+
+	const isAxiosError = (error: unknown): error is AxiosError => {
+		return (error as AxiosError).isAxiosError !== undefined;
+	};
+
+	const restoreButtonClick = async (data: INewPassword) => {
+		try {
+			if (baseUrl) {
+				await ResetPasswordWithEmail(baseUrl, data);
+			} else {
+				return router.push(MainPath.ServerError);
+			}
+		} catch (error) {
+			if (
+				isAxiosError(error) &&
+				error.response &&
+				error.response.status >= ApiResponseCode.SERVER_ERROR_STATUS_MIN &&
+				error.response.status < ApiResponseCode.SERVER_ERROR_STATUS_MAX
+			) {
+				return router.push(MainPath.ServerError);
+			}
+		}
 	};
 
 	return (

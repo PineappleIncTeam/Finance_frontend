f6646871ec7a790faf9dfa41a9456b72288fd30c
DenisBardashevich <ackapbinka@mail.ru>
2025-01-13 00:55:01 +0300

    fix: correct adaptation

--- src/app/(privateProfile)/savings/page.tsx
+++ src/app/(privateProfile)/savings/page.tsx
@@ -30,39 +30,41 @@ function Savings() {
 			<div className={style.savingsPageContainer}>
 				<form className={style.savingsFormContentWrapper}>
 					<h1 className={style.headerTitle}>Накопления</h1>
-					<div className={style.savingsByDateConteiner}>
-						<div className={style.totalAmountWrapper}>
-							<p className={style.totalAmountWrapper__savings}>Общая сумма накоплений </p>
-							<p className={style.totalAmountWrapper__sum}>4 112 500 ₽</p>
+					<div className={style.savingsFormContentWrapperContainer}>
+						<div className={style.savingsByDateConteiner}>
+							<div className={style.totalAmountWrapper}>
+								<p className={style.totalAmountWrapper__savings}>Общая сумма накоплений </p>
+								<p className={style.totalAmountWrapper__sum}>4 112 500 ₽</p>
+							</div>
+							<div className={style.dateSelectionWrapper}>
+								<p className={style.dateSelectionWrapper__description}>Выбор даты</p>
+								<AppInput control={control} label={"Выбор даты"} type={InputTypeList.Date} name={"date"} />
+							</div>
 						</div>
-						<div className={style.dateSelectionWrapper}>
-							<p className={style.dateSelectionWrapper__description}>Выбор даты</p>
-							<AppInput control={control} label={"Выбор даты"} type={InputTypeList.Date} name={"date"} />
-						</div>
-					</div>
-					<div className={style.savingsDetailsContainer}>
-						<div className={style.savingsDetailsContainer__category}>
-							<Select
-								name={"expenses"}
-								label={"Накопления"}
-								options={["Обучение ребенка", "Машина", "Квартира", "Отпуск 2024"]}
-							/>
-						</div>
-						<div className={style.savingsDetailsContainer__sum}>
-							<AppInput
-								control={control}
-								label={"Сумма"}
-								type={InputTypeList.Number}
-								name={"number"}
-								placeholder={"0.00 ₽"}
-							/>
-						</div>
+						<div className={style.savingsDetailsContainer}>
+							<div className={style.savingsDetailsContainer__category}>
+								<Select
+									name={"expenses"}
+									label={"Накопления"}
+									options={["Обучение ребенка", "Машина", "Квартира", "Отпуск 2024"]}
+								/>
+							</div>
+							<div className={style.savingsDetailsContainer__sum}>
+								<AppInput
+									control={control}
+									label={"Сумма"}
+									type={InputTypeList.Number}
+									name={"number"}
+									placeholder={"0.00 ₽"}
+								/>
+							</div>
 
-						{/* styleName переделать! */}
+							{/* styleName переделать! */}
 
-						<Button content={"Добавить"} styleName={"buttonForIncome__disabled"}>
-							<PlusIcon classNames={style.addButtonIcon} />
-						</Button>
+							<Button content={"Добавить"} styleName={"buttonForIncome__disabled"}>
+								<PlusIcon classNames={style.addButtonIcon} />
+							</Button>
+						</div>
 					</div>
 					<div>Категория, цель---</div>
 				</form>

18864209b1c22953d25bfea936eeb2ca738bbff5
Zhdanovich <Zhiv80@mail.ru>
2025-01-14 21:49:53 +0300

    Fix: add fonts subsets

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,9 +1,11 @@
 import { Rubik, Manrope } from "next/font/google";
 
 export const rubik = Rubik({
+	subsets: ["cyrillic", "latin"],
 	display: "swap",
 });
 
 export const manrope = Manrope({
+	subsets: ["cyrillic", "latin"],
 	display: "swap",
 });

afa482ff68d14e9df35953e3def00556cf88de23
Faz-R <FazaR92@gmail.com>
2025-01-22 15:05:31 +0300

    reforge: move arthive item component to personal folder

--- src/components/userProfileLayout/userProfileArchive/userProfileArchive.tsx
+++ src/components/userProfileLayout/userProfileArchive/userProfileArchive.tsx
@@ -1,31 +1,7 @@
-import { useState } from "react";
-
-import { DeleteIcon } from "../../../assets/script/expenses/DeleteIcon";
-import ResetIcon from "../../../assets/script/privateProfileNavBar/ResetIcon";
 import { archiveList } from "../../../mocks/PrivateProfileArchive";
-import { SimpleTooltip } from "../simpleTooltip/simpleTooltip";
-
-import { IArchiveItem } from "../../../types/common/ComponentsProps";
+import { ArchiveItem } from "../userProfileArchiveItem/userProfileArchiveItem";
 
 import style from "./userProfileArchive.module.scss";
-
-const ArchiveItem = ({ archiveItemValue }: IArchiveItem) => {
-	const [isTooltipShown, setIsTooltipShown] = useState<boolean>(false);
-	return (
-		<div className={style.archiveItem}>
-			<p className={style.archiveItem__title}>{archiveItemValue}</p>
-			<div className={style.archiveItemIconsWrap}>
-				<DeleteIcon classNames={style.archiveItem__icon} />
-				<div className={style.resetWrapper}>
-					<div onMouseOver={() => setIsTooltipShown(true)} onMouseLeave={() => setIsTooltipShown(false)}>
-						<ResetIcon classNames={style.archiveItem__icon} color={"var(--color-very-dark-grayish-blue)"} />
-					</div>
-					<SimpleTooltip open={isTooltipShown} text={"Восстановить"} className={style.archiveItem__tooltip} />
-				</div>
-			</div>
-		</div>
-	);
-};
 
 export const Archive = () => {
 	return (

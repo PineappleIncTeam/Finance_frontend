ce3fbf2bd6743f99f5d8cc0db0c81bd38b455867
NausAlexus <naus.lexus@mail.ru>
2025-01-27 23:21:41 +0300

    feat: format code

--- src/app/(privateProfile)/analytics/page.tsx
+++ src/app/(privateProfile)/analytics/page.tsx
@@ -14,7 +14,6 @@ import { InputTypeList } from "../../../helpers/Input";
 import styles from "./analytics.module.scss";
 
 function Analytics() {
-
 	const { control } = useForm<IExpensesInputForm>({
 		defaultValues: {
 			sum: "",
@@ -30,141 +29,141 @@ function Analytics() {
 			background: "--color-dark-spring-green",
 			title: "Внезапная покупка",
 			valuePercent: "1.00",
-			valueRub: 1300.01
+			valueRub: 1300.01,
 		},
 		{
 			id: 2,
 			background: "--color-lavender-blush",
 			title: "Стрижка",
 			valuePercent: "3.00",
-			valueRub: 3900.02
+			valueRub: 3900.02,
 		},
 		{
 			id: 3,
 			background: "--color-sage-green",
 			title: "Бассейн",
 			valuePercent: "2.50",
-			valueRub: 3250.02
+			valueRub: 3250.02,
 		},
 		{
 			id: 4,
 			background: "--color-light-peach",
 			title: "Школа",
 			valuePercent: "1.26",
-			valueRub: 1638.83
+			valueRub: 1638.83,
 		},
 		{
 			id: 5,
 			background: "--color-burnt-orange",
 			title: "Еда",
 			valuePercent: "2.04",
-			valueRub: 2652.06
+			valueRub: 2652.06,
 		},
 		{
 			id: 6,
 			background: "--color-soft-tan",
 			title: "Плата жилья",
 			valuePercent: "11.70",
-			valueRub: 15271.09
+			valueRub: 15271.09,
 		},
 		{
 			id: 7,
 			background: "--color-peach",
 			title: "Ногти",
 			valuePercent: "0.30",
-			valueRub: 390.00
+			valueRub: 390.0,
 		},
 		{
 			id: 8,
 			background: "--color-dark-sea-green",
 			title: "Бензин",
 			valuePercent: "0.75",
-			valueRub: 975.56
+			valueRub: 975.56,
 		},
 		{
 			id: 9,
 			background: "--color-dusty-rose",
 			title: "Дорога работа",
 			valuePercent: "1.03",
-			valueRub: 1340.79
+			valueRub: 1340.79,
 		},
 		{
 			id: 10,
 			background: "--color-steel-blue",
 			title: "Юрист",
 			valuePercent: "7.00",
-			valueRub: 9110.05
+			valueRub: 9110.05,
 		},
 		{
 			id: 11,
 			background: "--color-sunny-yellow",
 			title: "Детский сад",
 			valuePercent: "12.40",
-			valueRub: 16192.09
+			valueRub: 16192.09,
 		},
 		{
 			id: 12,
 			background: "--color-peach-pink",
 			title: "Учебники",
 			valuePercent: "2.00",
-			valueRub: 2600.01
+			valueRub: 2600.01,
 		},
 		{
 			id: 13,
 			background: "--color-forest-green",
 			title: "Отпуск",
 			valuePercent: "4.95",
-			valueRub: 6437.57
+			valueRub: 6437.57,
 		},
 		{
 			id: 14,
 			background: "--color-olive-green",
 			title: "Театр",
 			valuePercent: "1.30",
-			valueRub: 1690.01
+			valueRub: 1690.01,
 		},
 		{
 			id: 15,
 			background: "--color-pale-lemon",
 			title: "Кредит",
 			valuePercent: "20.00",
-			valueRub: 26000.15
+			valueRub: 26000.15,
 		},
 		{
 			id: 16,
 			background: "--color-dark-olive",
 			title: "Страховка",
 			valuePercent: "0.40",
-			valueRub: 520.00
+			valueRub: 520.0,
 		},
 		{
 			id: 17,
 			background: "--color-golden-olive",
 			title: "Киберспорт",
 			valuePercent: "0.40",
-			valueRub: 520.00
+			valueRub: 520.0,
 		},
 		{
 			id: 18,
 			background: "--color-light-blue-gray",
 			title: "Путешествия",
 			valuePercent: "0.40",
-			valueRub: 520.00
+			valueRub: 520.0,
 		},
 		{
 			id: 19,
 			background: "--color-burnt-orange",
 			title: "Кино",
 			valuePercent: "0.40",
-			valueRub: 520.00
+			valueRub: 520.0,
 		},
 		{
 			id: 20,
 			background: "--color-bright-lime",
 			title: "Ипотека",
 			valuePercent: "7.37",
-			valueRub: 9586.33
-		}
+			valueRub: 9586.33,
+		},
 	];
 	const WINDOW_SIZE = 1440;
 	const MIN_ROW = 0;
@@ -174,19 +173,19 @@ function Analytics() {
 	const [itemsToShow, setItemsToShow] = useState(MAX_ROW);
 	const [displayMode, setDisplayMode] = useState("rub");
 
-    useEffect(() => {
-        const handleResize = () => {
-            setItemsToShow(window.innerWidth <= WINDOW_SIZE ? MIN_ROW : MAX_ROW);
-        };
+	useEffect(() => {
+		const handleResize = () => {
+			setItemsToShow(window.innerWidth <= WINDOW_SIZE ? MIN_ROW : MAX_ROW);
+		};
 
-        handleResize();
+		handleResize();
 
-        window.addEventListener("resize", handleResize);
+		window.addEventListener("resize", handleResize);
 
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
 	const handleDisplayChange = (event: ChangeEvent<HTMLInputElement>) => {
 		setDisplayMode(event.target.value);
@@ -194,9 +193,7 @@ function Analytics() {
 
 	return (
 		<div className={styles.analyticsPageWrap}>
-
 			<div className={styles.analyticsPageContainer}>
-
 				<h1 className={styles.headerTitle}>Аналитика</h1>
 
 				{/* <div className={styles.analyticsBlankPage}>
@@ -208,10 +205,13 @@ function Analytics() {
 				</div> */}
 
 				<div className={styles.analyticsPagesContent}>
-
 					<div className={styles.analyticsSelectContainer}>
 						<div className={styles.analyticsSelectOperation}>
-							<Select name={"expenses"} label={"Операции"} options={["Расходы", "Доходы", "Анализ доходов и расходов"]} />
+							<Select
+								name={"expenses"}
+								label={"Операции"}
+								options={["Расходы", "Доходы", "Анализ доходов и расходов"]}
+							/>
 						</div>
 						<div className={styles.analyticsSelectDateAndPeriod}>
 							<AppInput control={control} label={"Выбор даты"} type={InputTypeList.Date} name={"date"} />
@@ -250,7 +250,6 @@ function Analytics() {
 					</div>
 
 					<div className={styles.analyticsDiagramExpensesWrapper}>
-
 						<div className={styles.analyticsDiagramExpensesInfo}>
 							<p className={styles.analyticsDiagramExpensesInfo__title}>Общий расход</p>
 							<p className={styles.analyticsDiagramExpensesInfo__value}>130 000.75 ₽</p>
@@ -258,22 +257,22 @@ function Analytics() {
 						</div>
 
 						<div className={styles.analyticsDiagramExpenses}>
-
 							<div className={styles.diagramExpenses}></div>
 
 							<div className={styles.diagramExpensesBlock}>
-
 								<div className={styles.diagramExpensesBlockLeft}>
 									<ul className={styles.diagramExpensesBlockLeftItems}>
-										{DIAGRAM_ITEMS_DATA.slice(MIN_ROW, MAX_ROW).map(item => (
+										{DIAGRAM_ITEMS_DATA.slice(MIN_ROW, MAX_ROW).map((item) => (
 											<li key={item.id} className={styles.diagramExpensesBlockLeftItem}>
 												<div className={styles.diagramExpensesBlockLeftIconWrapper}>
-													<div className={styles.diagramExpensesBlockLeftIconWrapper__circle} style={{background: `var(${item.background})`}}></div>
+													<div
+														className={styles.diagramExpensesBlockLeftIconWrapper__circle}
+														style={{ background: `var(${item.background})` }}></div>
 													<p className={styles.diagramExpensesBlockLeftIconWrapper__text}>{item.title}</p>
 												</div>
 												<p className={styles.diagramExpensesBlockLeftItem__value}>
-                                                    {displayMode === "rub" ? `${item.valueRub} ₽` : `${item.valuePercent}%`}
-                                                </p>
+													{displayMode === "rub" ? `${item.valueRub} ₽` : `${item.valuePercent}%`}
+												</p>
 											</li>
 										))}
 									</ul>
@@ -281,30 +280,26 @@ function Analytics() {
 
 								<div className={styles.diagramExpensesBlockRight}>
 									<ul className={styles.diagramExpensesBlockRightItems}>
-										{DIAGRAM_ITEMS_DATA.slice(itemsToShow).map(item => (
+										{DIAGRAM_ITEMS_DATA.slice(itemsToShow).map((item) => (
 											<li key={item.id} className={styles.diagramExpensesBlockRightItem}>
 												<div className={styles.diagramExpensesBlockRightIconWrapper}>
-													<div className={styles.diagramExpensesBlockRightIconWrapper__circle} style={{background: `var(${item.background})`}}></div>
+													<div
+														className={styles.diagramExpensesBlockRightIconWrapper__circle}
+														style={{ background: `var(${item.background})` }}></div>
 													<p className={styles.diagramExpensesBlockRightIconWrapper__text}>{item.title}</p>
 												</div>
 												<p className={styles.diagramExpensesBlockRightItem__value}>
-                                                    {displayMode === "rub" ? `${item.valueRub} ₽` : `${item.valuePercent}%`}
-                                                </p>
+													{displayMode === "rub" ? `${item.valueRub} ₽` : `${item.valuePercent}%`}
+												</p>
 											</li>
 										))}
 									</ul>
 								</div>
-
 							</div>
-
 						</div>
-
 					</div>
-					
 				</div>
-
 			</div>
-
 		</div>
 	);
 }

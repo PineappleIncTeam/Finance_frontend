d32603e0cea6482ee04243edd9fc1df37fea8a5f
Zhdanovich <Zhiv80@mail.ru>
2025-02-13 18:57:21 +0300

    Fix: correct default values

--- src/utils/formHelpers.ts
+++ src/utils/formHelpers.ts
@@ -1,5 +1,5 @@
 import { FieldErrors } from "react-hook-form";
-import { passwordStrength, defaultOptions } from "check-password-strength";
+import { passwordStrength, Options } from "check-password-strength";
 
 import {
 	errorDefault,
@@ -22,9 +22,32 @@ enum ErrorTypes {
 	PATTERN = "pattern",
 }
 
-defaultOptions.forEach((item) => {
-	item.minLength = 6;
-});
+export const defaultOptions: Options<string> = [
+	{
+		id: 0,
+		value: "Too weak",
+		minDiversity: 0,
+		minLength: 0,
+	},
+	{
+		id: 1,
+		value: "Weak",
+		minDiversity: 2,
+		minLength: 6,
+	},
+	{
+		id: 2,
+		value: "Medium",
+		minDiversity: 4,
+		minLength: 10,
+	},
+	{
+		id: 3,
+		value: "Strong",
+		minDiversity: 4,
+		minLength: 12,
+	},
+];
 
 class FormHelpers {
 	getPasswordError = (errors: FieldErrors, password: string) => {

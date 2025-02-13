3befcd62e610fe904230d0c30020f3963efedd8f
Zhdanovich <Zhiv80@mail.ru>
2025-02-13 14:30:44 +0300

    Feat: install check_password_strength, add password checks, add password constants

--- src/helpers/authConstants.ts
+++ src/helpers/authConstants.ts
@@ -4,12 +4,22 @@ export const passwordPattern =
 	/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*[^a-zA-Z\d!#/\\='"<>{}@,.$%[\]^&*()~_|:;+?^-])[^\s]{6,50}$/;
 
 export const errorDefault = "";
-export const errorRequiredField = "Не заполнено обязательное поле";
-export const errorEmailIncorrect = "Не верный формат ввода почты";
-export const errorEmailExist = "Такого адреса не существует";
-export const errorPasswordIncorrect = "Не верный пароль";
-export const errorPasswordRules = "Пароль не соответствует требованиям безопасности";
-export const errorPasswordLength = "Пароль должен содержать не менее 6 знаков";
-export const errorPasswordUppercase = "Пароль должен содержать букву верхнего регистра";
-export const errorPasswordNumber = "Пароль должен содержать хотя бы одну цифру";
-export const errorPasswordRepeat = "Введенный пароль не совпадает";
+export const errorRequiredField = "Не заполнено обязательное поле.";
+export const errorEmailIncorrect = "Не верный формат ввода почты.";
+export const errorEmailExist = "Такого адреса не существует.";
+export const errorPasswordIncorrect = "Не верный пароль.";
+export const errorPasswordRules = "Пароль не соответствует требованиям безопасности.";
+export const errorPasswordLength = "Пароль должен содержать не менее 6 знаков.";
+export const errorPasswordUppercase = "Пароль должен содержать букву верхнего регистра.";
+export const errorPasswordNumber = "Пароль должен содержать хотя бы одну цифру.";
+export const errorPasswordRepeat = "Введенный пароль не совпадает.";
+export const errorPasswordMoreTwoSameSymbolsRepeat =
+	"Пароль не должен содержать более двух одинаковых символов подряд.";
+export const errorPasswordThreeNumbersRow = "Пароль не должен содержать более трех цифр подряд.";
+export const errorPasswordPrivateBirthDate = "Пароль не должен содержать личную информацию, такую как дата рождения.";
+export const errorPasswordStrengthTooWeak =
+	"Ваш пароль не соответствует требованиям безопасности. Пожалуйста, используйте минимум 6 символов, включая заглавные буквы, цифры и специальные символы.";
+export const errorPasswordStrengthWeak =
+	"Ваш пароль считается слабым. Рекомендуем использовать минимум 8 символов и включить специальные символы для повышения его надежности.";
+export const errorPasswordStrengthMedium =
+	"Ваш пароль считается средним. Для повышения надёжности рекомендуем использовать минимум 15 символов, строчные и заглавные буквы, цифры и включить специальные символы.";
--- src/utils/formHelpers.ts
+++ src/utils/formHelpers.ts
@@ -1,4 +1,5 @@
 import { FieldErrors } from "react-hook-form";
+import { passwordStrength, defaultOptions } from "check-password-strength";
 
 import {
 	errorDefault,
@@ -8,6 +9,12 @@ import {
 	errorPasswordNumber,
 	errorRequiredField,
 	passwordPattern,
+	errorPasswordMoreTwoSameSymbolsRepeat,
+	errorPasswordPrivateBirthDate,
+	errorPasswordThreeNumbersRow,
+	errorPasswordStrengthTooWeak,
+	errorPasswordStrengthWeak,
+	errorPasswordStrengthMedium,
 } from "../helpers/authConstants";
 
 enum ErrorTypes {
@@ -15,10 +22,13 @@ enum ErrorTypes {
 	PATTERN = "pattern",
 }
 
+defaultOptions.forEach((item) => {
+	item.minLength = 6;
+});
+
 class FormHelpers {
 	getPasswordError = (errors: FieldErrors, password: string) => {
 		const errorInfo = errors.password || {};
-
 		switch (errorInfo.type) {
 			case ErrorTypes.REQUIRED:
 				return errorRequiredField;
@@ -34,13 +44,74 @@ class FormHelpers {
 		const minPasswordLength = 6;
 		if (password.length < minPasswordLength) {
 			messages.push(errorPasswordLength);
-		} else if (password.length >= minPasswordLength && !/(?=.*\d)/.test(password)) {
+		}
+		this.checkPasswordStrength(password, messages);
+		this.checkPasswordSameSymbolsThreeNumbersPrivateBirth(password, messages);
+		if (password.length >= minPasswordLength && !/(?=.*\d)/.test(password)) {
 			messages.push(errorPasswordNumber);
 		} else if (!passwordPattern.test(password)) {
 			messages.push(errorPasswordIncorrect);
 		}
 		if (messages.length === 0) {
 			return null;
+		}
+		return messages.join(" ");
+	};
+
+	checkPasswordStrength = (password: string, messages: string[]) => {
+		if (passwordStrength(password, defaultOptions).value === "Too weak") {
+			messages.push(errorPasswordStrengthTooWeak);
+		}
+		if (passwordStrength(password, defaultOptions).value === "Weak") {
+			messages.push(errorPasswordStrengthWeak);
+		}
+		if (passwordStrength(password, defaultOptions).value === "Medium") {
+			messages.push(errorPasswordStrengthMedium);
+		}
+		return messages.join(" ");
+	};
+
+	checkPasswordSameSymbolsThreeNumbersPrivateBirth = (password: string, messages: string[]) => {
+		const passArray = password.split("");
+		for (let i = 0; i < password.length; i++) {
+			if (passArray[i] === passArray[i + 1]) {
+				messages.push(errorPasswordMoreTwoSameSymbolsRepeat);
+			}
+			if (+passArray[i + 1] - +passArray[i] == 1 && +passArray[i + 2] - +passArray[i + 1] == 1) {
+				messages.push(errorPasswordThreeNumbersRow);
+			}
+			if (
+				!isNaN(
+					Date.parse(
+						passArray[i] +
+							passArray[i + 1] +
+							"-" +
+							passArray[i + 2] +
+							passArray[i + 3] +
+							"-" +
+							passArray[i + 4] +
+							passArray[i + 5] +
+							passArray[i + 6] +
+							passArray[i + 7],
+					),
+				) ||
+				!isNaN(
+					Date.parse(
+						passArray[i] +
+							passArray[i + 1] +
+							passArray[i + 2] +
+							passArray[i + 3] +
+							"-" +
+							passArray[i + 4] +
+							passArray[i + 5] +
+							"-" +
+							passArray[i + 6] +
+							passArray[i + 7],
+					),
+				)
+			) {
+				messages.push(errorPasswordPrivateBirthDate);
+			}
 		}
 		return messages.join(" ");
 	};

067caac86d44f35491c1dbed3e0c19463bb352c2
AlexMakovetskiy <bo5634302@gmail.com>
2025-02-17 23:45:19 +0300

    refactor: remove magic numbers is generateRandomColor util function

--- src/utils/generateRandomColor.tsx
+++ src/utils/generateRandomColor.tsx
@@ -1,21 +1,33 @@
 function generateRandomColors(numColors: number): string[] {
 	const colors: string[] = [];
 
-	const isDarkOrLightColor = (color: string): boolean => {
-		const r = parseInt(color.slice(1, 3), 16);
-		const g = parseInt(color.slice(3, 5), 16);
-		const b = parseInt(color.slice(5, 7), 16);
+	const greenFirstSymbolIndex = 3;
+	const blueFirstSymbolIndex = 5;
+	const blueLastSymbolIndex = 7;
 
-		const average = (r + g + b) / 3;
-		return average < 50 || average > 205;
+	const colorCount = 3;
+	const maximalDarkColorValue = 50;
+	const minimalLightColorValue = 205;
+
+	const isDarkOrLightColor = (color: string): boolean => {
+		const r = parseInt(color.slice(1, greenFirstSymbolIndex), 16);
+		const g = parseInt(color.slice(greenFirstSymbolIndex, blueFirstSymbolIndex), 16);
+		const b = parseInt(color.slice(blueFirstSymbolIndex, blueLastSymbolIndex), 16);
+
+		const average = (r + g + b) / colorCount;
+		return average < maximalDarkColorValue || average > minimalLightColorValue;
 	};
+
+	const colorSalt = 16777215;
+	const radixValue = 16;
+	const minimalStringLength = 6;
 
 	while (colors.length < numColors) {
 		const color =
 			"#" +
-			Math.floor(Math.random() * 16777215)
-				.toString(16)
-				.padStart(6, "0");
+			Math.floor(Math.random() * colorSalt)
+				.toString(radixValue)
+				.padStart(minimalStringLength, "0");
 		if (!isDarkOrLightColor(color)) {
 			colors.push(color);
 		}

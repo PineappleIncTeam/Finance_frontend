c06982d55beba7fc92adf081e663261b71245528
DenisBardashevich <ackapbinka@mail.ru>
2025-02-26 17:32:32 +0300

    Fix: email validation

--- src/ui/authInput/AuthInput.tsx
+++ src/ui/authInput/AuthInput.tsx
@@ -11,39 +11,56 @@ import showPassword from "../../assets/pages/signUp/showPassword.svg";
 
 import styles from "./AuthInput.module.scss";
 
+const validateEmail = (value: string | boolean) => {
+	const stringValue = String(value).trim();
+  
+	const invalidStartChars = /^[\s.-]/;
+	if (invalidStartChars.test(stringValue)) {
+	  return "Неверный формат ввода почты";
+	}
+	return true;
+  };
+
 const AuthInput = ({ label, type, placeholder, autoComplete, subtitle, error, ...props }: IAuthInput) => {
-	const { field, fieldState } = useController<TAuthInputForm>(props);
-	const [passwordType, setPasswordType] = useState<InputTypeList>(InputTypeList.Password);
-	const togglePasswordVisibility = () =>
-		setPasswordType(passwordType === InputTypeList.Password ? InputTypeList.Text : InputTypeList.Password);
+  const { field, fieldState } = useController<TAuthInputForm>({
+    ...props,
+    rules: {
+      validate: (value) => validateEmail(value)
+    }
+  });
 
-	const value = typeof field.value === "boolean" ? String(field.value) : field.value;
+  
+  const [passwordType, setPasswordType] = useState<InputTypeList>(InputTypeList.Password);
+  const togglePasswordVisibility = () =>
+    setPasswordType(passwordType === InputTypeList.Password ? InputTypeList.Text : InputTypeList.Password);
 
-	return (
-		<div className={styles.inputWrap}>
-			<label className={styles.inputWrap__label}>{label}</label>
-			<div className={cn(styles.inputWrap__element, { [styles.inputWrap__element_error]: error })}>
-				<input
-					{...field}
-					type={type === InputTypeList.Password ? passwordType : type}
-					placeholder={placeholder}
-					className={styles.inputWrap__input}
-					autoComplete={autoComplete}
-					value={value}
-				/>
-				{type === InputTypeList.Password && (
-					<button
-						onClick={togglePasswordVisibility}
-						className={styles.inputWrap__passwordEye}
-						type={InputTypeList.Button}>
-						<Image src={showPassword} alt="Toggle visibility" />
-					</button>
-				)}
-			</div>
-			{fieldState.error && <p className={styles.inputWrap__error}>{fieldState.error.message || (error as string)}</p>}
-			{subtitle && !error && <p className={styles.inputWrap__subtitle}>{subtitle}</p>}
-		</div>
-	);
+  const value = typeof field.value === "boolean" ? String(field.value) : field.value;
+
+  return (
+    <div className={styles.inputWrap}>
+      <label className={styles.inputWrap__label}>{label}</label>
+      <div className={cn(styles.inputWrap__element, { [styles.inputWrap__element_error]: fieldState.error })}>
+        <input
+          {...field}
+          type={type === InputTypeList.Password ? passwordType : type}
+          placeholder={placeholder}
+          className={styles.inputWrap__input}
+          autoComplete={autoComplete}
+          value={value}
+        />
+        {type === InputTypeList.Password && (
+          <button
+            onClick={togglePasswordVisibility}
+            className={styles.inputWrap__passwordEye}
+            type={InputTypeList.Button}>
+            <Image src={showPassword} alt="Toggle visibility" />
+          </button>
+        )}
+      </div>
+      {fieldState.error && <p className={styles.inputWrap__error}>{fieldState.error.message || (error as string)}</p>}
+      {subtitle && !error && <p className={styles.inputWrap__subtitle}>{subtitle}</p>}
+    </div>
+  );
 };
 
 export default AuthInput;

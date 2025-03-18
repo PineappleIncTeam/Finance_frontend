70626b954efb1dd1ee620fa351415de3b62d0ffb
DenisBardashevich <ackapbinka@mail.ru>
2025-03-18 20:00:47 +0300

    fix

--- src/services/api/categories/DeletepostUserCategories.ts
+++ src/services/api/categories/DeletepostUserCategories.ts
@@ -3,14 +3,22 @@ import axios from "axios";
 import { DeleteUserCategoriesEndpoint } from "./apiConstants";
 
 export const deleteUserCategories = async (
-    baseURL: string,
-    categoryId: number,
-  ) => {
-    return await axios(
-      `${baseURL}/${DeleteUserCategoriesEndpoint.replace("{id}", categoryId.toString())}`,
-      {
-        method: "DELETE",
-        withCredentials: true,
-      }
-    );
-  };
+  baseURL: string,
+  categoryId: number,
+) => {
+  console.log("Отправка запроса на удаление категории с ID:", categoryId); // Логирование
+  try {
+      const response = await axios(
+          `${baseURL}/${DeleteUserCategoriesEndpoint.replace("{id}", categoryId.toString())}`,
+          {
+              method: "DELETE",
+              withCredentials: true,
+          }
+      );
+      console.log("Ответ от сервера:", response.data); // Логирование ответа
+      return response;
+  } catch (error) {
+      console.error("Ошибка при удалении категории:", error); // Логирование ошибки
+      throw error;
+  }
+};

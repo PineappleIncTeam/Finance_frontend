6cf37b682b1ac502f47eaf0ffec2b485734c865e
Zhdanovich <Zhiv80@mail.ru>
2025-03-18 16:48:05 +0300

    Fix: change api const and query

--- src/services/api/userProfile/RemoveExpensesCategory.tsx
+++ src/services/api/userProfile/RemoveExpensesCategory.tsx
@@ -3,9 +3,8 @@ import axios from "axios";
 import { removeCategoryExpensesEndPoint } from "../auth/apiConstants";
 
 export const RemoveExpensesCategory = async (baseUrl: string, id: string) => {
-	return await axios(`${baseUrl}/${removeCategoryExpensesEndPoint}`, {
+	return await axios(`${baseUrl}/${removeCategoryExpensesEndPoint}/${id}`, {
 		method: "DELETE",
-		data: id,
 		headers: {
 			"Content-Type": "application/json",
 		},

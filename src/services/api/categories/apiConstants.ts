f780b6e5214dad74aaa955606b1cbc5a1fc93cf7
DenisBardashevich <ackapbinka@mail.ru>
2025-03-23 21:43:33 +0300

    fix

--- src/services/api/categories/apiConstants.ts
+++ src/services/api/categories/apiConstants.ts
@@ -1,2 +1,2 @@
-export const UserCategoriesEndpoint = "api/v1/categories/";
+export const UserCategoriesEndpoint = "api/v1/categories/all/";
 export const DeleteUserCategoriesEndpoint = "api/v1/categories/{id}/";

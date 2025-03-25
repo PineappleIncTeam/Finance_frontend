05c2ffdc1e03eb7fbd47f9b423a5c15dd25894b0
Zhdanovich <Zhiv80@mail.ru>
2025-03-25 21:26:12 +0300

    Fix: change mocks

--- src/mocks/ExpensesTransaction.ts
+++ src/mocks/ExpensesTransaction.ts
@@ -4,28 +4,28 @@ import { IExpenseTransaction } from "../types/components/ComponentsTypes";
 export const expensesTransactions: IExpenseTransaction[] = [
 	{
 		date: "24 января 2024, Чт",
-		target: "Обучение ребёнка",
+		name: "Обучение ребёнка",
 		amount: "- 50 000.00 ₽",
 	},
 	{
 		date: "24 января 2024, Чт",
-		target: "Ремонт машины",
+		name: "Ремонт машины",
 		amount: "- 10 000.00 ₽",
 	},
 	{
 		date: "24 января 2024, Чт",
-		target: "ЖКХ",
+		name: "ЖКХ",
 		amount: "- 7 000.00 ₽",
 	},
 	{
 		date: "24 января 2024, Чт",
-		target: "Охрана школы",
+		name: "Охрана школы",
 		amount: "- 1 000.00 ₽",
 	},
 
 	{
 		date: "24 января 2024, Чт",
-		target: "Накопления на квартиру",
+		name: "Накопления на квартиру",
 		amount: "- 100 500.00 ₽",
 	},
 ];

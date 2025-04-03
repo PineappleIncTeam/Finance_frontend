b333982f01d7e2c4545fb72a57cdef7c2c746c77
Zhdanovich <Zhiv80@mail.ru>
2025-04-03 16:22:51 +0300

    Feat: add modal and negative balance check to the modal

--- src/components/userProfileLayout/editTransaction/editTransaction.tsx
+++ src/components/userProfileLayout/editTransaction/editTransaction.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 import { InputTypeList } from "../../../helpers/Input";
@@ -12,6 +13,8 @@ import { ButtonType } from "../../../helpers/buttonFieldValues";
 import Button from "../../../ui/Button/button";
 import Title from "../../../ui/title/Title";
 
+import { NegativeBalanceModal } from "../recordDeleteResponse/negativeBalance/negativeBalance";
+
 import styles from "./editTransaction.module.scss";
 
 export const EditTransactionModal = ({ open, id, request, cancelEdit }: IEditTransaction) => {
@@ -23,37 +26,48 @@ export const EditTransactionModal = ({ open, id, request, cancelEdit }: IEditTra
 		delayError: 200,
 	});
 
+	const [isNegativeBalanceModalShown, setIsNegativeBalanceModalShown] = useState<boolean>(false);
+	const interval = 2000;
+
 	const onSubmit = async (data: IEditTransactionForm) => {
 		if (id !== null && data !== null) {
+			if (data.amount === 0 || data.amount < 0) {
+				setIsNegativeBalanceModalShown(true);
+				setTimeout(() => setIsNegativeBalanceModalShown(false), interval);
+				return;
+			}
 			await request(id, data);
 		}
 	};
 
 	return (
-		<dialog open={open} className={styles.backgroundModal}>
-			<div onClick={(e) => e.stopPropagation()} role="textbox" className={styles.changeAmountModalContainer}>
-				<form className={styles.changeAmountModalContainer__form} onSubmit={handleSubmit(onSubmit)}>
-					<Title title={"Изменение суммы"} />
-					<div className={styles.changeAmountFormData}>
-						<p className={styles.changeAmountFormData__label}>Введите новое числовое значение</p>
-						<AppInput
-							control={control}
-							label={"Введите новое числовое значение"}
-							name={"amount"}
-							type={InputTypeList.Number}
-							placeholder={"100.00"}
-						/>
-					</div>
-					<div className={styles.buttonsContainer}>
-						<Button variant={ButtonType.Outlined} onClick={() => cancelEdit()}>
-							Отменить
-						</Button>
-						<Button variant={ButtonType.Faded} type={InputTypeList.Submit}>
-							Изменить
-						</Button>
-					</div>
-				</form>
-			</div>
-		</dialog>
+		<>
+			<dialog open={open} className={styles.backgroundModal}>
+				<div onClick={(e) => e.stopPropagation()} role="textbox" className={styles.changeAmountModalContainer}>
+					<form className={styles.changeAmountModalContainer__form} onSubmit={handleSubmit(onSubmit)}>
+						<Title title={"Изменение суммы"} />
+						<div className={styles.changeAmountFormData}>
+							<p className={styles.changeAmountFormData__label}>Введите новое числовое значение</p>
+							<AppInput
+								control={control}
+								label={"Введите новое числовое значение"}
+								name={"amount"}
+								type={InputTypeList.Number}
+								placeholder={"100.00"}
+							/>
+						</div>
+						<div className={styles.buttonsContainer}>
+							<Button variant={ButtonType.Outlined} onClick={() => cancelEdit()}>
+								Отменить
+							</Button>
+							<Button variant={ButtonType.Faded} type={InputTypeList.Submit}>
+								Изменить
+							</Button>
+						</div>
+					</form>
+				</div>
+			</dialog>
+			{isNegativeBalanceModalShown && <NegativeBalanceModal open={isNegativeBalanceModalShown} />}
+		</>
 	);
 };

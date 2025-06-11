644c89be672396bfea3902d1f3dec4eb80d43c55
Zhdanovich <Zhiv80@mail.ru>
2025-06-11 10:36:12 +0300

    Feat: add pkce challenge lib and generate code func

--- src/helpers/sdkVkId.ts
+++ src/helpers/sdkVkId.ts
@@ -1,9 +1,18 @@
 import * as VKID from "@vkid/sdk";
+import pkceChallenge from "pkce-challenge";
+
+import { UserProfilePath } from "../services/router/routes";
+import { getCorrectBaseUrl } from "../utils/baseUrlConverter";
+
+const generateCodeChallenge = async (): Promise<string> => {
+	const challenge = await pkceChallenge();
+	return challenge.codeChallenge;
+};
 
 export const vkIdConfig = VKID.Config.init({
 	app: Number(process.env.CLIENT_ID),
-	redirectUrl: String(process.env.REDIRECT_URL),
-	state: process.env.STATE,
-	codeChallenge: process.env.CODE_CHALLENGE,
+	redirectUrl: `${getCorrectBaseUrl()}${UserProfilePath.ProfitMoney}`,
+	state: String(generateCodeChallenge()),
+	codeChallenge: String(generateCodeChallenge()),
 	scope: "emil phone",
 });

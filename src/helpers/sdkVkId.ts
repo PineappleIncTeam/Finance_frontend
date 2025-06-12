11404b2bb862c04d6ba5fc603db0d1bc4cff960d
AlexMakovetskiy <108091268+AlexMakovetskiy@users.noreply.github.com>
2025-06-12 11:51:36 +0300

    Update sdkVkId.ts

--- src/helpers/sdkVkId.ts
+++ src/helpers/sdkVkId.ts
@@ -5,7 +5,7 @@ import { getCorrectBaseUrl } from "../utils/baseUrlConverter";
 import { generateCodeChallenge, generateState } from "../utils/generateAuthTokens";
 
 export const vkIdConfig = VKID.Config.init({
-	app: Number(process.env.CLIENT_ID),
+	app: Number(process.env.NEXT_PUBLIC_VK_APP_ID),
 	redirectUrl: `${getCorrectBaseUrl()}${UserProfilePath.ProfitMoney}`,
 	state: generateState(),
 	codeChallenge: String(generateCodeChallenge()),

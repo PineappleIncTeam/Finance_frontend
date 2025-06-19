c42ad7aa36e8003cf2f39f54f1c00dcd48d64bca
Kiril Hantsuk <kirilhantsuk@gmail.com>
2025-06-19 20:17:14 +0300

    Added an application hint when using a medium level password

--- src/ui/authInput/AuthInput.tsx
+++ src/ui/authInput/AuthInput.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
-import { useController } from "react-hook-form";
+import React, { useState, useEffect } from "react";
 import cn from "classnames";
 import Image from "next/image";
+import { useController } from "react-hook-form";
 
-import { TAuthInputForm, IAuthInput } from "../../types/common/UiKitProps";
+import { passwordStrength } from "check-password-strength";
 
 import { InputTypeList } from "../../helpers/Input";
-
+import { defaultOptions } from "../../helpers/passwordStrengthOption";
+import { TAuthInputForm, IAuthInput } from "../../types/common/UiKitProps";
 import showPassword from "../../assets/pages/signUp/showPassword.svg";
 
 import styles from "./AuthInput.module.scss";
@@ -14,8 +15,29 @@ import styles from "./AuthInput.module.scss";
 const AuthInput = ({ label, type, placeholder, autoComplete, subtitle, error, ...props }: IAuthInput) => {
 	const { field, fieldState } = useController<TAuthInputForm>(props);
 	const [passwordType, setPasswordType] = useState<InputTypeList>(InputTypeList.Password);
+	const [customMessage, setCustomMessage] = useState<string | null>(null);
 	const togglePasswordVisibility = () =>
 		setPasswordType(passwordType === InputTypeList.Password ? InputTypeList.Text : InputTypeList.Password);
+
+	useEffect(() => {
+		if (type === InputTypeList.Password && field.value && typeof field.value === "string") {
+			try {
+				const result = passwordStrength(field.value, defaultOptions);
+				if (result.value === "Medium") {
+					setCustomMessage(
+						"Ваш пароль соответствует требованиям безопасности. Для повышения надёжности рекомендуем использовать не менее 15 символов, заглавные буквы и специальные символы"
+					);
+				} else {
+					setCustomMessage(null);
+				}
+			} catch (e) {
+				console.error("Ошибка проверки пароля:", e);
+				setCustomMessage(null);
+			}
+		} else {
+			setCustomMessage(null);
+		}
+	}, [field.value, type]);
 
 	const value = typeof field.value === "boolean" ? String(field.value) : field.value;
 
@@ -42,6 +64,12 @@ const AuthInput = ({ label, type, placeholder, autoComplete, subtitle, error, ..
 				)}
 			</div>
 			{fieldState.error && <p className={styles.inputWrap__error}>{fieldState.error.message || (error as string)}</p>}
+			{!fieldState.error && !error && customMessage && (
+				<p className={cn(styles.inputWrap__subtitle, styles.inputWrap__subtitle_green)}>
+					{customMessage}
+				</p>
+			)}
+			
 			{subtitle && !error && <p className={styles.inputWrap__subtitle}>{subtitle}</p>}
 		</div>
 	);

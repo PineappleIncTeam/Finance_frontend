9de95d536cf174b1b2cad0c73d12e5c198a769e5
Zhdanovich <Zhiv80@mail.ru>
2025-06-27 10:16:58 +0300

    Feat: create component CurtainVk

--- src/components/mainLayout/curtainVk/curtainVk.tsx
+++ src/components/mainLayout/curtainVk/curtainVk.tsx
@@ -1,12 +1,14 @@
 import * as VKID from "@vkid/sdk";
 
-import { ICurtainVk } from "../types/common/ComponentsProps";
+import { ICurtainVk } from "../../../types/common/ComponentsProps";
 
-export const curtainVK = (handleError: ICurtainVk) => {
+export const CurtainVK = (handleError: ICurtainVk) => {
 	const floatingOneTap = new VKID.FloatingOneTap();
 	floatingOneTap
 		.render({ appName: "freenance-app", scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS })
 		.on(VKID.WidgetEvents.ERROR, handleError);
 
 	VKID.Auth.login();
+
+	return <></>;
 };

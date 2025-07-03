304c7327eee6161e201541798482588358229574
AlexMakovetskiy <108091268+AlexMakovetskiy@users.noreply.github.com>
2025-07-03 17:09:31 +0300

    feat: add strong password message

--- src/ui/authInput/AuthInput.tsx
+++ src/ui/authInput/AuthInput.tsx
@@ -9,14 +9,15 @@ import { InputTypeList } from "../../helpers/Input";
 import { defaultOptions } from "../../helpers/passwordStrengthOption";
 import { TAuthInputForm, IAuthInput } from "../../types/common/UiKitProps";
 import showPassword from "../../assets/pages/signUp/showPassword.svg";
-import { errorPasswordStrengthMedium } from "../../helpers/authConstants";
+import { errorPasswordStrengthMedium, errorPasswordStrengthStrong } from "../../helpers/authConstants";
 
 import styles from "./AuthInput.module.scss";
 
 const AuthInput = ({ label, type, placeholder, autoComplete, subtitle, error, ...props }: IAuthInput) => {
 	const { field, fieldState } = useController<TAuthInputForm>(props);
 	const [passwordType, setPasswordType] = useState<InputTypeList>(InputTypeList.Password);
-	const [isMediumPassword, setIsMediumPassword] = useState(false);
+	const [isMediumPassword, setIsMediumPassword] = useState<boolean>(false);
+	const [isStrongPassword, setIsStrongPassword] = useState<boolean>(false);
 	const togglePasswordVisibility = () =>
 		setPasswordType(passwordType === InputTypeList.Password ? InputTypeList.Text : InputTypeList.Password);
 
@@ -25,9 +26,11 @@ const AuthInput = ({ label, type, placeholder, autoComplete, subtitle, error, ..
 			try {
 				const result = passwordStrength(field.value, defaultOptions);
 				setIsMediumPassword(result.value === "Medium");
+				setIsStrongPassword(result.value === "Strong");
 				// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 			} catch (error: unknown) {
 				setIsMediumPassword(false);
+				setIsStrongPassword(false);
 			}
 		}
 	}, [field.value, type]);
@@ -60,6 +63,11 @@ const AuthInput = ({ label, type, placeholder, autoComplete, subtitle, error, ..
 			{!fieldState.error && !error && isMediumPassword && (
 				<p className={cn(styles.inputWrap__subtitle, styles.inputWrap__subtitle_green)}>
 					{errorPasswordStrengthMedium}
+				</p>
+			)}
+			{!fieldState.error && !error && isStrongPassword && (
+				<p className={cn(styles.inputWrap__subtitle, styles.inputWrap__subtitle_green)}>
+					{errorPasswordStrengthStrong}
 				</p>
 			)}
 

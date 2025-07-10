5a0d150d2872ffe84a6e0a2a4e5e183e4167f837
AlexMakovetskiy <108091268+AlexMakovetskiy@users.noreply.github.com>
2025-07-10 21:22:35 +0300

    fix: add codeVerifier function

--- src/helpers/sdkVkId.ts
+++ src/helpers/sdkVkId.ts
@@ -2,13 +2,13 @@ import * as VKID from "@vkid/sdk";
 
 import { UserProfilePath } from "../services/router/routes";
 import { getCorrectBaseUrl } from "../utils/baseUrlConverter";
-import { generateCodeChallenge, generateState } from "../utils/generateAuthTokens";
+import { generateCodeVerifier, generateState } from "../utils/generateAuthTokens";
 
 export const vkIdConfig = VKID.Config.init({
 	app: Number(process.env.NEXT_PUBLIC_VK_APP_ID),
 	redirectUrl: `${getCorrectBaseUrl()}${UserProfilePath.ProfitMoney}`,
 	state: generateState(),
-	codeChallenge: String(generateCodeChallenge()),
+	codeChallenge: String(generateCodeVerifier()),
 	scope: "email phone",
 	responseMode: VKID.ConfigResponseMode.Callback,
 });

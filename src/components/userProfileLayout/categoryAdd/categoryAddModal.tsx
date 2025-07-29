d3a78c0f1d90489e6f1d00d2dce936c012c595ca
Zhdanovich <Zhiv80@mail.ru>
2025-07-29 16:57:50 +0300

    Fix: remove unnecessary paragraphs with styles

--- src/components/userProfileLayout/categoryAdd/categoryAddModal.tsx
+++ src/components/userProfileLayout/categoryAdd/categoryAddModal.tsx
@@ -46,7 +46,6 @@ export const CategoryAddModal = ({ open, request, onCancelClick }: IAddCategory)
 				<form className={styles.addCategoryFormContainer} onSubmit={handleSubmit(onSubmit)}>
 					<Title title={"Добавление категории"} />
 					<div className={styles.addCategoryFormData}>
-						<p className={styles.addCategoryFormData__label}>Введите название категории</p>
 						<AppInput
 							control={control}
 							label={"Введите название категории"}

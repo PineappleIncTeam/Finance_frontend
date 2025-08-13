427cfd2267e7763398e649e056a00be2bf370732
AlexMakovetskiy <108091268+AlexMakovetskiy@users.noreply.github.com>
2025-08-13 16:59:48 +0300

    Update apiConstants.ts

--- src/services/api/auth/apiConstants.ts
+++ src/services/api/auth/apiConstants.ts
@@ -2,7 +2,7 @@ export const mockLocalhostStr = "localhost";
 export const mockLocalhostUrl = "127.0.0.1";
 
 export const signupEndpoint = "api/v1/auth/users/";
-export const signInEndpoint = "api/v1/auth/login/";
+export const signInEndpoint = "api/v1/auth/token/login/";
 export const logoutEndpoint = "api/v1/auth/logout/";
 export const validateTokenEndpoint = "api/v1/auth/users/me/";
 export const userActivationEndpoint = "api/v1/auth/users/activation/";

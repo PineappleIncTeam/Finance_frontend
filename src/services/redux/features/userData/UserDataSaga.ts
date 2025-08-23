3780822c146d8b390d39bb13aa5f5e08594cf1ff
Kiril Hantsuk <kirilhantsuk@gmail.com>
2025-08-23 12:42:51 +0300

    commented file to avoid git error

--- src/services/redux/features/userData/UserDataSaga.ts
+++ src/services/redux/features/userData/UserDataSaga.ts
@@ -1,30 +1,30 @@
-import { call, put, takeLatest } from "redux-saga/effects";
+// import { call, put, takeLatest } from "redux-saga/effects";
 
-import { getUserData, updateUserData } from "../../../../services/api/auth/userApi";
+// import { getUserData, updateUserData } from "../../../../services/api/auth/userApi";
 
-import { userDataActions } from "./userDataActions";
+// import { userDataActions } from "./userDataActions";
 
-function* fetchUserDataSaga() {
-	try {
-		yield put(userDataActions.pending());
-		const response = yield call(getUserData);
-		yield put(userDataActions.fulfilled(response.data));
-	} catch (error: any) {
-		yield put(userDataActions.rejected(error.message));
-	}
-}
+// function* fetchUserDataSaga() {
+// 	try {
+// 		yield put(userDataActions.pending());
+// 		const response = yield call(getUserData);
+// 		yield put(userDataActions.fulfilled(response.data));
+// 	} catch (error: any) {
+// 		yield put(userDataActions.rejected(error.message));
+// 	}
+// }
 
-function* updateUserDataSaga(action: ReturnType<typeof userDataActions.update>) {
-	try {
-		yield put(userDataActions.pending());
-		const response = yield call(updateUserData, action.payload);
-		yield put(userDataActions.fulfilled(response.data));
-	} catch (error: any) {
-		yield put(userDataActions.rejected(error.message));
-	}
-}
+// function* updateUserDataSaga(action: ReturnType<typeof userDataActions.update>) {
+// 	try {
+// 		yield put(userDataActions.pending());
+// 		const response = yield call(updateUserData, action.payload);
+// 		yield put(userDataActions.fulfilled(response.data));
+// 	} catch (error: any) {
+// 		yield put(userDataActions.rejected(error.message));
+// 	}
+// }
 
-export function* watchUserData() {
-	yield takeLatest(userDataActions.pending, fetchUserDataSaga);
-	yield takeLatest(userDataActions.update, updateUserDataSaga);
-}
+// export function* watchUserData() {
+// 	yield takeLatest(userDataActions.pending, fetchUserDataSaga);
+// 	yield takeLatest(userDataActions.update, updateUserDataSaga);
+// }

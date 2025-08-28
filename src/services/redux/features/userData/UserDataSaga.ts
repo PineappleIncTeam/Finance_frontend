57ab5cb5f51c7d468530a0a9ba1c5f8f7074d801
Kiril Hantsuk <kirilhantsuk@gmail.com>
2025-08-28 20:34:40 +0300

    added types in yeild

--- src/services/redux/features/userData/UserDataSaga.ts
+++ src/services/redux/features/userData/UserDataSaga.ts
@@ -1,31 +1,32 @@
 import { call, put, takeLatest } from "redux-saga/effects";
+import { SagaIterator } from "redux-saga";
 
 import { getUserData } from "../../../api/auth/getUserData";
 import { updateUserData } from "../../../api/auth/updateUserData";
 
 import { userDataActions } from "./userDataActions";
 
-function* fetchUserDataSaga() {
+function* fetchUserDataSaga(): SagaIterator {
 	try {
 		yield put(userDataActions.pending());
-		const response = yield call(getUserData);
+		const response: { data: any } = yield call(getUserData);
 		yield put(userDataActions.fulfilled(response.data));
 	} catch (error: any) {
 		yield put(userDataActions.rejected(error.message));
 	}
 }
 
-function* updateUserDataSaga(action: ReturnType<typeof userDataActions.update>) {
+function* updateUserDataSaga(action: ReturnType<typeof userDataActions.update>): SagaIterator {
 	try {
 		yield put(userDataActions.pending());
-		const response = yield call(updateUserData, action.payload);
+		const response: { data: any } = yield call(updateUserData, action.payload);
 		yield put(userDataActions.fulfilled(response.data));
 	} catch (error: any) {
 		yield put(userDataActions.rejected(error.message));
 	}
 }
 
-export function* watchUserData() {
+export function* watchUserData(): SagaIterator {
 	yield takeLatest(userDataActions.pending, fetchUserDataSaga);
 	yield takeLatest(userDataActions.update, updateUserDataSaga);
 }

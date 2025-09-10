cb26b8ee7ab5cdca03148f533c008379a97382f8
Zhdanovich <Zhiv80@mail.ru>
2025-09-10 12:37:19 +0300

    feat: add id to Category Select

--- src/types/common/ComponentsProps.ts
+++ src/types/common/ComponentsProps.ts
@@ -116,7 +116,7 @@ export interface ICategorySelect<T extends FieldValues> {
 	options: ICategoryOption[] | ITarget[];
 	control: Control<T>;
 	name: Path<T>;
-	id?: number | string;
+	id?: Path<T>;
 	placeholder?: string;
 	onAddCategory: () => void;
 	onRemoveCategory?: (categoryId: number, categoryName: string) => void;

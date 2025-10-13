f41ef8f06344687faf58d25643ce6c563fa94c9b
Zhdanovich <Zhiv80@mail.ru>
2025-10-13 20:07:44 +0300

    feat: add buttons & styles

--- src/components/userProfileLayout/inactivityLogoutModal/inactivityLogoutModal.tsx
+++ src/components/userProfileLayout/inactivityLogoutModal/inactivityLogoutModal.tsx
@@ -1,4 +1,6 @@
+import { ButtonType } from "../../../helpers/buttonFieldValues";
 import { IInactivityLogoutModal } from "../../../types/components/ComponentsTypes";
+import Button from "../../../ui/Button/Button";
 
 import styles from "./inactivityLogoutModal.module.scss";
 
@@ -6,11 +8,19 @@ export default function InactivityLogoutModal({ open }: IInactivityLogoutModal) 
 	return (
 		<dialog open={open} className={styles.backgroundModal}>
 			<div onClick={(e) => e.stopPropagation()} role="textbox" className={styles.modalContainer}>
-				<p className={styles.modalContainer__subtitle}>Вы всё ещё здесь?</p>
+				<p className={styles.modalContainer__title}>Вы всё ещё здесь?</p>
 				<p className={styles.modalContainer__subtitle}>
 					Поскольку не было никаких действий в приложении, выход будет произведён менее чем через 2 минуты. Вы хотите
 					остаться?
 				</p>
+				<div className={styles.buttonsContainer}>
+					<Button variant={ButtonType.Contained} onClick={() => undefined}>
+						Выйти
+					</Button>
+					<Button variant={ButtonType.Outlined} onClick={() => undefined}>
+						Остаться
+					</Button>
+				</div>
 			</div>
 		</dialog>
 	);

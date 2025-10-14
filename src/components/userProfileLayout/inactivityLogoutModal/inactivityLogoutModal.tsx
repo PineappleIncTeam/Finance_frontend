3f0abe57fedc087588bbc2d880bdf6357c0db99a
Zhdanovich <Zhiv80@mail.ru>
2025-10-14 15:10:54 +0300

    feat: add 2 minute timer

--- src/components/userProfileLayout/inactivityLogoutModal/inactivityLogoutModal.tsx
+++ src/components/userProfileLayout/inactivityLogoutModal/inactivityLogoutModal.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { ButtonType } from "../../../helpers/buttonFieldValues";
 import { IInactivityLogoutModal } from "../../../types/components/ComponentsTypes";
 import Button from "../../../ui/Button/Button";
@@ -5,6 +7,28 @@ import Button from "../../../ui/Button/Button";
 import styles from "./inactivityLogoutModal.module.scss";
 
 export default function InactivityLogoutModal({ open }: IInactivityLogoutModal) {
+	const initialValue: number = 120;
+	const [time, setTime] = useState<number>(initialValue);
+	const interval: number = 1000;
+	const inMinuteSeconds: number = 60;
+
+	useEffect(() => {
+		if (time <= 0) {
+			return;
+		}
+		const intervalId: NodeJS.Timeout = setInterval(() => {
+			setTime(time - 1);
+		}, interval);
+
+		return () => clearInterval(intervalId);
+	}, [time]);
+
+	const formatTime = (seconds: number) => {
+		const minutes: number = Math.floor(seconds / inMinuteSeconds);
+		const remainingSeconds: number = seconds % inMinuteSeconds;
+		return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+	};
+
 	return (
 		<dialog open={open} className={styles.backgroundModal}>
 			<div onClick={(e) => e.stopPropagation()} role="textbox" className={styles.modalContainer}>
@@ -13,6 +37,7 @@ export default function InactivityLogoutModal({ open }: IInactivityLogoutModal) 
 					Поскольку не было никаких действий в приложении, выход будет произведён менее чем через 2 минуты. Вы хотите
 					остаться?
 				</p>
+				<p className={styles.modalContainer__timer}>{formatTime(time)}</p>
 				<div className={styles.buttonsContainer}>
 					<Button variant={ButtonType.Contained} onClick={() => undefined}>
 						Выйти

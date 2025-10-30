cab85c4a5aa3a521e3c58fcb4bd5450852f3f573
Zhdanovich <Zhiv80@mail.ru>
2025-10-30 08:54:17 +0300

    fix: increase setTimeout interval

--- src/app/(home)/changePassword/page.tsx
+++ src/app/(home)/changePassword/page.tsx
@@ -32,7 +32,7 @@ export default function ChangePassword() {
 
 	const baseUrl = String(env("NEXT_PUBLIC_BASE_URL") ?? "");
 
-	const secondsCount = 4000;
+	const interval = 7000;
 
 	const {
 		control,
@@ -57,10 +57,10 @@ export default function ChangePassword() {
 
 	const handleChangePasswordModal = () => {
 		setIsChangePasswordModalOpen(true);
-		setTimeout(async () => {
-			await setIsChangePasswordModalOpen(false);
+		setTimeout(() => {
+			setIsChangePasswordModalOpen(false);
 			router.push(MainPath.Login);
-		}, secondsCount);
+		}, interval);
 	};
 
 	const onSubmit = async (data: IChangePasswordForm) => {

c15ae7101212797330251fa083889c0f09f31b25
AlexMakovetskiy <108091268+AlexMakovetskiy@users.noreply.github.com>
2025-10-31 21:57:20 +0300

    build: update npm package versions

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-	turbopack: {
-		rules: {
-			"*.module.scss": {
-				loaders: ["sass-loader"],
-				as: "*.module.css",
-			},
-		},
-	},
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;

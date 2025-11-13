c172f195e50b7fb2281f419027f035d36f4c2bd1
Zhdanovich <Zhiv80@mail.ru>
2025-11-13 12:56:41 +0300

    feat: add get reports categories request, const, interfaces

--- src/services/api/auth/apiConstants.ts
+++ src/services/api/auth/apiConstants.ts
@@ -20,6 +20,7 @@ export const expensesCategoriesAllEndPoint = "api/v1/categories/all/";
 export const expensesCategoryTransactionsAllEndPoint = "api/v1/operations/all/";
 export const targetsEndPoint = "api/v1/targets";
 export const operationsEndPoint = "api/v1/operations";
+export const reportsCategoriesEndPoint = "api/v1/reports/categories";
 
 export const currencyEndpoint = "api/v1/currency/";
 

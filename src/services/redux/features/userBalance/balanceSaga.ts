e924d367e90d2174256a9433db7d8ef86d77cb41
Kiril Hantsuk <kirilhantsuk@gmail.com>
2025-11-15 23:25:13 +0300

    commented to push

--- src/services/redux/features/userBalance/balanceSaga.ts
+++ src/services/redux/features/userBalance/balanceSaga.ts
@@ -1,39 +1,39 @@
-import { call, put, takeLatest } from "redux-saga/effects";
+// import { call, put, takeLatest } from "redux-saga/effects";
 
-import { AxiosResponse } from "axios";
+// import { AxiosResponse } from "axios";
 
-import { balanceActions } from "../../../../types/redux/sagaActions/storeSaga.actions";
+// import { balanceActions } from "../../../../types/redux/sagaActions/storeSaga.actions";
 
-import { getUserBalance } from "../../../api/userProfile/getUserBalance";
+// import { getUserBalance } from "../../../api/userProfile/getUserBalance";
 
-import { mockBaseUrl } from "../../../../mocks/envConsts";
+// import { mockBaseUrl } from "../../../../mocks/envConsts";
 
-import { balanceSlice } from "./BalanceSlice";
+// import { balanceSlice } from "./BalanceSlice";
 
-interface IGetBalanceResponse {
-	current_balance: number;
-	error_message?: string;
-	error_code?: number;
-}
+// interface IGetBalanceResponse {
+// 	current_balance: number;
+// 	error_message?: string;
+// 	error_code?: number;
+// }
 
-export function* fetchBalanceSaga(
-	action: ReturnType<typeof balanceActions.pending>,
-): Generator<any, void, any> {
-	try {
-		const response: AxiosResponse<IGetBalanceResponse> = yield call(getUserBalance, mockBaseUrl);
+// export function* fetchBalanceSaga(
+// 	action: ReturnType<typeof balanceActions.pending>,
+// ): Generator<any, void, any> {
+// 	try {
+// 		const response: AxiosResponse<IGetBalanceResponse> = yield call(getUserBalance, mockBaseUrl);
 
-		yield put(
-			balanceSlice.actions.setBalance({
-				currentBalance: response.data.current_balance,
-			}),
-		);
+// 		yield put(
+// 			balanceSlice.actions.setBalance({
+// 				currentBalance: response.data.current_balance,
+// 			}),
+// 		);
 
-		yield put(balanceActions.fulfilled());
-	} catch (error: any) {
-		yield put(balanceActions.rejected(error.message));
-	}
-}
+// 		yield put(balanceActions.fulfilled());
+// 	} catch (error: any) {
+// 		yield put(balanceActions.rejected(error.message));
+// 	}
+// }
 
-export function* watchBalanceSaga() {
-	yield takeLatest(balanceActions.pending, fetchBalanceSaga);
-}
+// export function* watchBalanceSaga() {
+// 	yield takeLatest(balanceActions.pending, fetchBalanceSaga);
+// }

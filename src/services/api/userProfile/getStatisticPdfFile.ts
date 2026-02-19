2eadeaaa89f22e27696e177177a67eb37c314540
AlexMakovetskiy <108091268+AlexMakovetskiy@users.noreply.github.com>
2026-02-19 22:54:14 +0300

    feat: update analytics files requests

--- src/services/api/userProfile/getStatisticPdfFile.ts
+++ src/services/api/userProfile/getStatisticPdfFile.ts
@@ -6,13 +6,11 @@ import { IStatisticFilePayload } from "../../../types/pages/Analytics";
 export const getStatisticPdfFile = async (baseUrl: string, data: IStatisticFilePayload) => {
 	return await axios(`${baseUrl}/${getStatisticPdfFileEndpoint}`, {
 		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-		},
 		params: {
 			type: data.type,
 			days: data.days,
 		},
+		responseType: "text",
 		withCredentials: true,
 		withTokenRefresh: true,
 	});

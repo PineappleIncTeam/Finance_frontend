f249a152961b44bd12053479c5d6a6811980b079
Kiril Hantsuk <kirilhantsuk@gmail.com>
2026-03-10 21:38:10 +0300

    added a manifest file

---
+++ public/manifest.ts
@@ -0,0 +1,29 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: "Freenance",
+		short_name: "Freenance",
+		description: "Manage your finances easily and conveniently with Freenance.",
+		start_url: "/",
+		scope: "/",
+		display: "standalone",
+		orientation: "portrait",
+		background_color: "#ffffff",
+		theme_color: "#1a572e",
+		lang: "ru",
+		categories: ["finance", "productivity"],
+		icons: [
+			{
+				src: "/icon-192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/icon-512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+		],
+	};
+}
